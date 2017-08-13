4825e0661f46c0dcd423ec55f508fbf745a0a385
riaankleynhans <riaan@number7even.com>
2017-08-13 20:15:46 +0530

    Update installcomposer.sh

--- scripts/installcomposer.sh
+++ scripts/installcomposer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir /var/www/vhosts/emporium-voyage.com/public_html
 cd /var/www/vhosts/emporium-voyage.com/public_html
-wget https://s3-eu-west-1.amazonaws.com/emporium-voyage/env/live/.env
+wget https://s3-eu-west-1.amazonaws.com/emporium-voyage-live/env/live/.env
 chmod 755 .env
 chown www-data:www-data .env
 composer update -d /var/www/vhosts/emporium-voyage.com/public_html
@@ -10,6 +10,6 @@ sudo find /var/www/vhosts/emporium-voyage.com/public_html -type d -exec chmod 75
 sudo find /var/www/vhosts/emporium-voyage.com/public_html -type f -exec chmod 755 {} +
 chmod -R 777 /var/www/vhosts/emporium-voyage.com/public_html/storage
 mkdir /var/www/vhosts/emporium-voyage.com/public_html/public/uploads
-s3fs emporium-voyage /var/www/vhosts/emporium-voyage.com/public_html/public/uploads/ -o passwd_file=/etc/passwd-s3fs  -ouid=33,gid=33,allow_other,mp_umask=002
+s3fs emporium-voyage-live /var/www/vhosts/emporium-voyage.com/public_html/public/uploads/ -o passwd_file=/etc/passwd-s3fs  -ouid=33,gid=33,allow_other,mp_umask=002
 composer dump-autoload -d /var/www/vhosts/emporium-voyage.com/public_html
 service nginx restart
