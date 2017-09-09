c20e001f45e9e57fffd3132d5eedae1d0c490094
Ravinder Singh <ravinder@number7even.de>
2017-09-09 17:43:02 +0530

    updated

--- resources/views/container/index.blade.php
+++ resources/views/container/index.blade.php
@@ -218,96 +218,9 @@ div[data-load="left-side-tree"]{max-height: 600px;    overflow: auto;}
 								</div>
 							</div>
 							<div class="clear"></div>
+							<!-- Load Folders -->
 							<div id="folders_data_list"><p style="padding-top: 30px; text-align: center;">Loading...</p></div>
-							<?php /*@if(!empty($rowData))
-								<div id="container_sortable">	
-									@foreach($rowData as $row)
-										@if($row['ftype']=='folder')
-										<div class="gallery-box ui-state-default">
-											<div class="caption folder">
-												<a href="{{ URL::to('folders/').'/'.$row['id'].'?show='.$showType }}">{{(strlen($row['name']) > 8) ? substr($row['name'],0,8)."~" : $row['name']}}</a>
-												<img src="{{URL::to('uploads/images/information.png')}}" style="cursor:pointer;" class="screenshot" rel="{{($row['title']!='')?$row['title']:''}}" rel2="{{($row['description']!='')?$row['description']:''}}" title="{{$row['name']}}" />
-												@if($row['assign_front']=='yes')
-													<img src="{{URL::to('uploads/images/activated.png')}}" style="cursor:pointer; margin-left:5px;" title="Click to Deactivate Frontend" onclick="frontend_grid(this,'folder','{{$row['id']}}',1);" />
-												@else
-													<img src="{{URL::to('uploads/images/not_activated.png')}}" style="cursor:pointer; margin-left:5px;" title="Click to Activate Frontend" onclick="frontend_grid(this,'folder','{{$row['id']}}',0);" />
-												@endif
-											</div>
-											
-											<?php $folderPic = ($row['cover_img']!='')? URL::to('uploads/folder_cover_imgs/thumb_'.$row['cover_img']): URL::to('uploads/images/folder_big.png');
-											
-												$folderPicPopup = ($row['cover_img']!='')? URL::to('uploads/folder_cover_imgs/format_'.$row['cover_img']): URL::to('uploads/images/folder_big.png');
-												
-												$img_name = ($row['cover_img']!='')? 'format_'.$row['cover_img']: 'folder_big.png';
-											?>
-											
-											<div class="thumb folder" style="background: url('{{ $folderPic }}') no-repeat  center center; background-size:100px auto;" >
-												<a href="{{ URL::to('folders/').'/'.$row['id'].'?show='.$showType }}" rel="{{$folderPicPopup}}" rel2="{{$img_name}}" title="{{$row['name']}}" class="screenshot">&nbsp;</a>
-											</div>
-											<div class="info">
-												<label><input type="checkbox" name="compont[]" id="compont" value="folder-{{$row['id']}}" class="no-border check-files ff"></label>
-												<div>{{$row['foldercount']}} folder</div>
-												<div>{{$row['filecount']}} file</div>
-											</div>
-										</div>
-										@else
-										<div class="gallery-box ui-state-default">
-											<?php $expFile = explode('.',$row['name']); 
-											$imgclass = end($expFile);
-											$ext = end($expFile);
-											$isImg = 0;
-											if($ext=="jpg" || $ext=="png" || $ext=="gif" || $ext=="bmp" || $ext=="jpeg" || $ext=="JPG") { 
-												$imgclass = "img"; $isImg=1;
-												$imgfancy[] = $row['imgsrc'].$row['name'];
-											}
-											$fname = ($row['file_display_name']!='')? $row['file_display_name']: $row['name'];
-											?>
-											
-											<div class="caption {{$imgclass}}">
-												<a class="lfile" href="{{ URL::to('files/view/').'/'.$fid.'/'.$row['id'].'?show='.$showType }}" title="{{$row['name']}}">{{strlen($fname) > 5 ? substr($fname,0,2)."~.".$ext : $fname}}</a>
-												<img src="{{URL::to('uploads/images/information.png')}}" style="cursor:pointer;" class="screenshot" rel="{{($row['title']!='')?$row['title']:''}}" rel2="{{($row['description']!='')?$row['description']:''}}" title="{{$fname}}" />
-												@if($row['assign_front']=='yes')
-													<img src="{{URL::to('uploads/images/activated.png')}}" style="cursor:pointer; margin-left:5px;" title="Click to Deactivate Frontend" onclick="frontend_grid(this,'file','{{$row['id']}}',1);" />
-												@else
-													<img src="{{URL::to('uploads/images/not_activated.png')}}" style="cursor:pointer; margin-left:5px;" title="Click to Activate Frontend" onclick="frontend_grid(this,'file','{{$row['id']}}',0);" />
-												@endif
-												@if($row['assign_lightbox']=='yes')
-													<img src="{{URL::to('uploads/images/activated_lightbox.png')}}" style="cursor:pointer; margin-left:5px;" title="Click to Deactivate Lightbox" onclick="lightbox_grid(this,'{{$row['id']}}',1);" />
-												@else
-													<img src="{{URL::to('uploads/images/not_activated_lightbox.png')}}" style="cursor:pointer; margin-left:5px;" title="Click to Activate Lightbox" onclick="lightbox_grid(this,'{{$row['id']}}',0);" />
-												@endif
-											</div>
-											<?php if($ext=="pdf")
-											{
-												$imgclass = "bigpdf";
-											}?>
-											<div class="thumb cinner{{$imgclass}}" <?php if($isImg==1) { ?> style="background: url('{{URL::to('uploads/thumbs/').'/thumb_'.$fid.'_'.$row['name']}}') no-repeat  center center; background-size:100px auto;" <?php } ?>>
-												<a href="{{ URL::to('files/view/').'/'.$fid.'/'.$row['id'].'?show='.$showType }}" class="screenshot fancybox-buttons" rel="{{URL::to('uploads/thumbs/').'/format_'.$fid.'_'.$row['name']}}" title="{{$fname}}" rel2="{{$fname}}" data-fancybox-group="button">
-													&nbsp;
-												</a>
-											</div>
-											<div class="info">
-												<label><input type="checkbox" value="file-{{$row['id']}}-{{$ext}}" name="compont[]" id="compont" class="no-border check-files ff"></label>
-												@if(($row['tiff_files']!='') && !empty($row['tiff_files']))
-													<div style="border-right:none;">
-													@foreach($row['tiff_files'] as $tif)
-														<?php $expTif = explode('.',$tif->file_name); 
-														$imgval = $expTif[1]; ?>
-														<a href="{{ URL::to('tfiles/view/').'/'.$fid.'/'.$tif->id.'?show='.$showType }}">&nbsp;&nbsp;{{strtoupper($imgval)}}&nbsp;&nbsp;</a>
-													@endforeach
-													</div>
-												@endif
-												<div class="action">
-													<a href="{{$row['imgsrc'].$row['name']}}" title="Download" download="{{$row['name']}}">
-														<img src="{{URL::to('uploads/images/bullet_download.png')}}" width="16" height="16" alt="Download">
-													</a>
-												</div>
-											</div>
-										</div>
-										@endif
-									@endforeach
-								</div>
-							@endif*/ ?>
+							
 						@elseif($showType=="list")
 							<div class="gallery-select-all">
 								<label style="float:left;">
@@ -345,101 +258,9 @@ div[data-load="left-side-tree"]{max-height: 600px;    overflow: auto;}
 									</form>
 								</div>
 							</div>
+							<!-- Load Folders -->
 							<div id="folders_data_list"><p style="padding-top: 30px; text-align: center;">Loading...</p></div>
-							<?php /*<table class="list js-dynamitable table table-bordered">
-								<thead>
-									<tr>
-										<th class="check" width="5%">
-											<label><input type="checkbox" value="1" id="check_all" class="check-all"></label>
-										</th>
-										<th width="25%">Name</th>
-										<th width="5%"></th>
-										<th width="5%"></th>
-										<th width="20%">Title</th>
-										<th width="20%">Description</th>
-										<th width="15%">File Type</th>
-										<th width="5%">Status</th>
-									</tr>
-									<tr>
-										<th></th>
-										<th><input type="text" class="js-filter  form-control" value="" /></th>
-										<th></th>
-										<th></th>
-										<th><input type="text" class="js-filter  form-control" value="" /></th>
-										<th><input type="text" class="js-filter  form-control" value="" /></th>
-										<th><input type="text" class="js-filter  form-control" value="" /></th>
-										<th></th>
-									</tr>
-								</thead>
-								<tbody>
-									@if(!empty($rowData))
-									{{--*/ //$al=1 /*--}}
-										//@foreach($rowData as $row)
-										//{{--*/ $alt = ($al%2==0)? 'alt' : ''; /*--}}
-											/*@if($row['ftype']=='folder')
-												<tr class="{{$alt}}">
-													<td class="check">
-														<label><input type="checkbox" name="compont[]" id="compont" value="folder-{{$row['id']}}" class="no-border check-files ff"></label>
-													</td>
-													<td class="rowtitle folder">
-														<a href="{{ URL::to('folders/').'/'.$row['id'].'?show='.$showType }}">{{strlen($row['name']) > 22 ? substr($row['name'],0,20)."~" : $row['name']}}</a>
-													</td>
-													<td></td>
-													<td></td>
-													<td>{{($row['title']!='')?$row['title']:''}}</td>
-													<td>{{($row['description']!='')?substr($row['description'],0,20):''}}</td>
-													<td>Folder</td>
-													<td style="text-align:center;">@if($row['assign_front']=='yes')
-															<img src="{{URL::to('uploads/images/activated.png')}}" style="cursor:pointer; margin-left:5px;" title="Click to Deactivate" onclick="frontend_grid(this,'folder','{{$row['id']}}',1);" />
-														@else
-															<img src="{{URL::to('uploads/images/not_activated.png')}}" style="cursor:pointer; margin-left:5px;" title="Click to Activate" onclick="frontend_grid(this,'folder','{{$row['id']}}',0);" />
-														@endif
-													</td>
-												</tr>
-											@else
-												<?php $expFile = explode('.',$row['name']); 
-												$imgclass = end($expFile);
-												$ext = end($expFile);
-												$isImg = 0;
-												if($ext=="jpg" || $ext=="png" || $ext=="gif" || $ext=="bmp" || $ext=="jpeg" || $ext=="JPG") {
-													$imgclass = "img"; $isImg=1;
-													$imgfancy[] = $row['imgsrc'].$row['name'];
-												}
-												$fname = ($row['file_display_name']!='')? $row['file_display_name']: $row['name'];
-												?>
-												<tr class="{{$alt}}">
-													<td class="check">
-														<label><input type="checkbox" value="file-{{$row['id']}}-{{$ext}}" name="compont[]" id="compont" class="no-border check-files ff"></label>
-													</td>
-													<td class="rowtitle {{$imgclass}}">
-														<a href="{{ URL::to('files/view/').'/'.$fid.'/'.$row['id'].'?show='.$showType }}" title="{{$row['name']}}">{{strlen($fname) > 22 ? substr($fname,0,18)."~.".$ext : $fname}}</a>
-													</td>
-													<td style="text-align:center;">
-														<a href="#" class="screenshot fancybox-buttons" rel="{{URL::to('uploads/thumbs/').'/format_'.$fid.'_'.$row['name']}}" title="{{$fname}}" data-fancybox-group="button">
-															<img src="{{URL::to('uploads/images/magnifier.png')}}" width="16" height="16" alt="Preview">
-														</a>
-													</td>
-													<td style="text-align:center;">
-														<a href="{{$row['imgsrc'].$row['name']}}" title="Download" download="{{$row['name']}}">
-															<img src="{{URL::to('uploads/images/bullet_download.png')}}" width="16" height="16" alt="Download">
-														</a>
-													</td>
-													<td>{{($row['title']!='')?$row['title']:''}}</td>
-													<td>{{($row['description']!='')?substr($row['description'],0,20):''}}</td>
-													<td>{{$filType[$ext]}}</td>
-													<td style="text-align:center;">@if($row['assign_front']=='yes')
-															<img src="{{URL::to('uploads/images/activated.png')}}" style="cursor:pointer; margin-left:5px;" title="Click to Deactivate" onclick="frontend_grid(this,'file','{{$row['id']}}',1);" />
-														@else
-															<img src="{{URL::to('uploads/images/not_activated.png')}}" style="cursor:pointer; margin-left:5px;" title="Click to Activate" onclick="frontend_grid(this,'file','{{$row['id']}}',0);" />
-														@endif
-													</td>
-												</tr>
-											@endif
-											{{--*$al++ /*--}}
-										@endforeach
-									@endif
-								</tbody>
-							</table> */?>
+							
 						@endif
 					</div>
 				</div>
@@ -2314,7 +2135,7 @@ div[data-load="left-side-tree"]{max-height: 600px;    overflow: auto;}
 						$('#folders_data_list').hide();
 						$('#folders_data_list').html(data);
 						$('#folders_data_list').fadeIn('slow');
-						$('#folders_data_list').find('input[type=checkbox]').iCheck('icheckbox_square-green');
+						$('#folders_data_list').find('input[type=checkbox]').iCheck({checkboxClass: 'icheckbox_square-green'});
 						$('#breadcrumb_line').html($('#folders_data_list').find('#get-breadcrumb').html());
 					}
 				});
