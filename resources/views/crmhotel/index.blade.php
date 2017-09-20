81a19b5ad1e537974a76e3e4becd0567b46d4ec8
puneetsachdeva-number7even <puneet@number7even.de>
2017-09-20 18:27:31 +0530

    email driver settings

--- resources/views/crmhotel/index.blade.php
+++ resources/views/crmhotel/index.blade.php
@@ -67,31 +67,20 @@
 						@endif 
 					@endif
 				@endforeach
-				<th width="150" >{{ Lang::get('core.btn_action') }}</th>
+				<th width="180" >{{ Lang::get('core.btn_action') }}</th>
 			  </tr>
         </thead>
 
         <tbody>        						
             @foreach ($rowData as $row)
+			{{--*/ $hotel_slug = \SiteHelpers::seoUrl($row->hotel_name); /*--}}
                 <tr>
 					<td width="30"> {{ ++$i }} </td>
 					<td width="50"><input type="checkbox" class="ids" name="ids[]" value="{{ $row->id }}" />  </td>									
-				 @foreach ($tableGrid as $field)
-					 @if($field['view'] =='1')
-					 	<?php $limited = isset($field['limited']) ? $field['limited'] :''; ?>
-					 	@if(SiteHelpers::filterColumn($limited ))
-						 <td>
-						 {{--*/ $vfield = $field['field']; /*--}}
-						 	@if($field['attribute']['image']['active'] =='1')
-								{!! SiteHelpers::showUploadedFile($row->$vfield,$field['attribute']['image']['path']) !!}
-							@else	
-								{{--*/ $conn = (isset($field['conn']) ? $field['conn'] : array() ) /*--}}
-								{!! SiteHelpers::gridDisplay($row->$vfield,$field['field'],$conn) !!}	
-							@endif						 
-						 </td>
-						@endif	
-					 @endif					 
-				 @endforeach
+					<td> <a target="_blank" href="{{URL::to($hotel_slug)}}">{{$row->hotel_name}}</a> </td>
+					<td> {{$row->hotel_email}} </td>
+					<td> {{$row->hotel_main_phone}} </td>
+					<td> {{$row->hotel_manager_name}} </td>
 				 <td>
 					 	@if($access['is_detail'] ==1)
 						<a href="{{ URL::to('crmhotel/show/'.$row->id.'?return='.$return)}}" class="tips btn btn-xs btn-primary" title="{{ Lang::get('core.btn_view') }}"><i class="fa  fa-search "></i></a>
@@ -102,6 +91,7 @@
 						<a  href="{{ URL::to('properties/update/'.$row->propr_id.'?return='.$return) }}" class="tips btn btn-xs btn-success" title="property module"><i class="fa fa-cogs"></i></a>	
 						<a  href="{{ URL::to('properties_settings/'.$row->propr_id.'/types') }}" class="tips btn btn-xs btn-success" title="reservations"><i class="fa fa-cogs"></i></a>					
 						<a  href="javascript:void(0);" class="tips btn btn-xs btn-success" title="Email" onclick="sendemails_crmhotels('{{$row->id}}');"><i class="fa fa-envelope-o"></i></a>
+						<a  href="{{(strpos($row->hotel_website, 'http') !== false) ? $row->hotel_website : 'http://'.$row->hotel_website }}" class="tips btn btn-xs btn-success" title="website"><i class="fa fa-globe"></i></a>
 				</td>				 
                 </tr>
 				
@@ -118,7 +108,6 @@
 </div>	
 	</div>	  
 </div>
-
 <!--Email popup start-->
 <div id="email-page" class="popup">
 	<div class="popup-inner">
@@ -133,9 +122,9 @@
 			</div>
 			<div class="email-tabs-outer">
 				<div class="white-bar-menu-outer">
-					<ul class="nav nav-pills email-pop-up-tabs">
-						<li class="active"><a data-toggle="pill" href="#email-tab1">{{ Lang::get('core.crmhotel_email_popup_emailtab') }}</a></li>
-						<li><a data-toggle="pill" href="#email-tab2">{{ Lang::get('core.crmhotel_email_popup_emaillist') }}</a></li>
+					<ul class="nav nav-tabs email-pop-up-tabs">
+						<li class="active"><a data-toggle="tab" href="#email-tab1">{{ Lang::get('core.crmhotel_email_popup_emailtab') }}</a></li>
+						<li><a data-toggle="tab" href="#email-tab2">{{ Lang::get('core.crmhotel_email_popup_emaillist') }}</a></li>
 					</ul>
 				</div>
 				<div class="clearfix"></div>
@@ -150,7 +139,7 @@
 								</div>
 								<div class="col-md-6">
 									<div class="email-form-outer">
-										{!! Form::open(['url' => '#','method'=>'post','id'=>'emailcrm']) !!}
+										{!! Form::open(['url' => '#','method'=>'post','id'=>'emailcrm', 'enctype'=>'multipart/form-data']) !!}
 											<input type="hidden" name="crmId_email_popup" id="crmId_email_popup" value="" />
 											<input type="hidden" name="propertyid_email_popup" id="propertyid_email_popup" value="" />
 											<div class="form-group">
@@ -214,7 +203,6 @@ $(document).ready(function(){
         $(this).parent().parent().fadeOut("slow");
         $("body").removeClass("fixed");
     });
-	
 });	
 
 function sendemails_crmhotels(crmid)
@@ -265,6 +253,7 @@ function sendemails_crmhotels(crmid)
 
 function submitcrmemail()
 {
+	$('#message_email_popup').val($('.note-editable').html());
 	var form_data = new FormData(document.getElementById('emailcrm'));
 	$.ajax({
 	  method: "post",
@@ -280,7 +269,7 @@ function submitcrmemail()
 		  if(data.status=='success'){
 			$("#email-page").fadeOut("slow");
 			$("body").removeClass("fixed");
-			html +='<div class="alert alert-danger fade in block-inner">';
+			html ='<div class="alert alert-danger fade in block-inner">';
 			html +='<button data-dismiss="alert" class="close" type="button">×</button>';
 			html +='<i class="icon-checkmark-circle"></i> Email Sent! </div>';
 			$('.page-content-wrapper #formerrors').html(html);
