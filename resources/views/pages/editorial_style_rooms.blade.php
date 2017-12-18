64b6644d98bded7817f6aaafe12f8a0cefa22108
harman <harman@number7even.de>
2017-12-18 14:47:07 +0530

    rooms gallery pop up

--- resources/views/pages/editorial_style_rooms.blade.php
+++ resources/views/pages/editorial_style_rooms.blade.php
@@ -179,7 +179,7 @@ foreach ($propertyDetail['typedata'] as $type) {
       <div class="modal-dialog VegasModelDialog">
 
         <!-- Modal content-->
-        <div class="modal-content VegasModelDialog">
+        <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal">&times;</button>
             <h4 class="modal-title">Modal Header</h4>
