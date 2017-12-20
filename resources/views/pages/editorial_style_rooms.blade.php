2a639dbb0e8ead987c0b1181289a409b918fb242
puneetsachdeva-number7even <puneet@number7even.de>
2017-12-20 17:45:47 +0530

    place rel attribute

--- resources/views/pages/editorial_style_rooms.blade.php
+++ resources/views/pages/editorial_style_rooms.blade.php
@@ -115,7 +115,7 @@
                 @for($rimg1=0; $rimg1 < $divd2; $rimg1++)
 
                 <div style="height: 370.688px; width: 100%;" class="foto1 clio1" rel="clio" data-image="{{\ImageCache::make($propertyDetail['roomimgs'][$type->id]['imgsrc_dir'].$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg1]->file_name,100,1000,null)}}">
-                    <a data-popup-id="detail-page-rooms-popup-{{$type->id}}"  class="roomimagdetail_view" href="javascript:void(0);">
+                    <a data-popup-id="detail-page-rooms-popup-{{$type->id}}"  class="roomimagdetail_view" href="javascript:void(0);" rel="{{$type->id}}">
                         <img style="height: 370.688px; width: 659px;" src="{{\ImageCache::make($propertyDetail['roomimgs'][$type->id]['imgsrc_dir'].$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg1]->file_name,100,1000,null)}}" rel="1" alt="{{$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg1]->file_name}}">
                     </a>
                 </div>
@@ -123,7 +123,7 @@
 
                 @for($rimg2=$rimg1; $rimg2 < $totimg; $rimg2++)
                 <div style="height: 370.688px;" class="foto2 clio2 " rel="clio" data-image="{{\ImageCache::make($propertyDetail['roomimgs'][$type->id]['imgsrc_dir'].$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg2]->file_name,100,1000,null)}}">
-                    <a data-popup-id="detail-page-rooms-popup-{{$type->id}}"  class="roomimagdetail_view" href="javascript:void(0);">
+                    <a data-popup-id="detail-page-rooms-popup-{{$type->id}}"  class="roomimagdetail_view" href="javascript:void(0);" rel="{{$type->id}}">
                         <img style="height: 370.688px;" src="{{\ImageCache::make($propertyDetail['roomimgs'][$type->id]['imgsrc_dir'].$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg2]->file_name,100,1000,null)}}" rel="2" alt="{{$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg2]->file_name}}">
                     </a>
                 </div>
@@ -172,14 +172,14 @@
                 @for($rimg1=0; $rimg1 < $divd2; $rimg1++)
 
                 <div style="height: 370.688px;" class="foto2 clio3" rel="clio" data-image="{{\ImageCache::make($propertyDetail['roomimgs'][$type->id]['imgsrc_dir'].$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg1]->file_name,100,1000,null)}}">
-                    <a data-popup-id="detail-page-rooms-popup-{{$type->id}}"  class="roomimagdetail_view" href="javascript:void(0);">
+                    <a data-popup-id="detail-page-rooms-popup-{{$type->id}}"  class="roomimagdetail_view" href="javascript:void(0);" rel="{{$type->id}}">
                         <img style="height: 370.688px; width: 659px;" src="{{\ImageCache::make($propertyDetail['roomimgs'][$type->id]['imgsrc_dir'].$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg1]->file_name,100,1000,null)}}" rel="3" alt="{{$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg1]->file_name}}">
                     </a>
                 </div>
                 @endfor
                 @for($rimg2=$rimg1; $rimg2 < $totimg; $rimg2++)
                 <div style="height: 370.688px; width: 100%;" class="foto1 clio4" rel="clio" data-image="{{\ImageCache::make($propertyDetail['roomimgs'][$type->id]['imgsrc_dir'].$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg2]->file_name,100,1000,null)}}">
-                    <a data-popup-id="detail-page-rooms-popup-{{$type->id}}"  class="roomimagdetail_view" href="javascript:void(0);">
+                    <a data-popup-id="detail-page-rooms-popup-{{$type->id}}"  class="roomimagdetail_view" href="javascript:void(0);" rel="{{$type->id}}">
                         <img style="height: 370.688px;" src="{{\ImageCache::make($propertyDetail['roomimgs'][$type->id]['imgsrc_dir'].$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg2]->file_name,100,1000,null)}}" rel="4" alt="{{$propertyDetail['roomimgs'][$type->id]['imgs'][$rimg1]->file_name}}">
                     </a>
                 </div>
