1e8a2f85bd7f8e86423b0e73a5c30c9f8adac214
Ravinder Singh <ravinder@number7even.de>
2017-12-21 19:42:03 +0530

    updated loader css

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -929,7 +929,7 @@ url: "{{ URL::to('filter_search_destionation')}}",
             $(".grid-page-category-slider-container").html( sliderHTML );
             $('.uk-slideshow:first-child .ai-uk-animation .bh-slideshow-thumbnail-split-preview').delay(3000).fadeIn(5000);
             $('.ai-uk-animation:first .bh-slideshow-thumbnail-split-preview').addClass('uk-animation-fade');
-        html += '<div class="row">';
+        html += '<div class="row animate-bottom">';
         var p = 1;
         var node_no = 1;
         var total_rows = 0;
@@ -1720,6 +1720,58 @@ url: "{{ URL::to('filter_search_destionation')}}",
                     <script>
                                             
                                         </script> 
+
+
+           <style>
+/* Center the loader */
+#loader {
+  
+
+  z-index: 1;
+  width: 30px;
+  height: 30px;
+ 
+  border: 4px solid #f0f0f0;
+  border-radius: 50%;
+  border-top: 4px solid #000;
+  width: 30px;
+  height: 30px;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+/* Add animation to "page content" */
+.animate-bottom {
+  position: relative;
+  -webkit-animation-name: animatebottom;
+  -webkit-animation-duration: 1s;
+  animation-name: animatebottom;
+  animation-duration: 1s;
+  display: none;
+}
+
+@-webkit-keyframes animatebottom {
+  from { bottom:-100px; opacity:0; display: block; } 
+  to { bottom:0px; opacity:1 ; }
+}
+
+@keyframes animatebottom { 
+  from{ bottom:-100px; opacity:0 ;  display: block;} 
+  to{ bottom:0; opacity:1 }
+}
+
+
+</style>                             
 
     </body>
 </html>
