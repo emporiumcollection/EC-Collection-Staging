64385021df6587fc4581680d9391f991847d3449
harman <harman@number7even.de>
2017-12-21 10:07:29 +0530

    rooms gallery pop up

--- resources/views/pages/editorial_style_rooms.blade.php
+++ resources/views/pages/editorial_style_rooms.blade.php
@@ -235,21 +235,21 @@
 								</div>
 							</div>
 							{{--*/ $relatimg = URL::to('uploads/property_imgs_thumbs/front_property_'.$relatedproperties[0]->folder_id.'_'.$relatedproperties[0]->file_name); /*--}}
-							<div class="new-hotel-image" style="background-image: url('{{$relatimg}}')">
-								<div class="new-hotels-image-tittle">
-									<h2 class="new-hotel-name">{{$relatedproperties[0]->property_name}}</h2>
-									<div class=" new-hotel-add">
-										<p>New York City</p>
-										<p>United States</p>
-									</div>
-								</div>
-								<div class="clearfix"></div>
-								<div class="new-hotel-view-more-btn">
-									<a class="" href="{{URL::to($relatedproperties[0]->property_slug)}}">
-										View Hotel
-									</a>
-								</div>
-							</div>
+							<div class="new-hotel-image" style="background-image:url(http://www.emporium-voyage.com/uploads/property_imgs_thumbs/xfront_property_4505_Emporium-Voyage-Hotel-Zoo-Berlin00017.jpg.pagespeed.ic.nY-effTxow.jpg)">
+													<div class="new-hotels-image-tittle">
+														<h2 class="new-hotel-name">Hotel Zoo Berlin </h2>
+														<div class=" new-hotel-add">
+															<p>New York City</p>
+															<p>United States</p>
+														</div>
+													</div>
+													<div class="clearfix"></div>
+													<div class="new-hotel-view-more-btn">
+														<a class="" href="http://www.emporium-voyage.com/hotel-zoo-berlin-" tabindex="0">
+															View Hotel
+														</a>
+													</div>
+												</div>
 							
 							<a class="bootom-view-next-btn" href="{{URL::to($relatedproperties[0]->property_slug)}}">
 								Visit All Hotels DOI
