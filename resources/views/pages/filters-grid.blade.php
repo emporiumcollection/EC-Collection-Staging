3f0f6893af10ae9e6f34e8e783a4f855f88d8119
harman <harman@number7even.de>
2017-12-23 15:38:35 +0530

    Phone email

--- resources/views/pages/filters-grid.blade.php
+++ resources/views/pages/filters-grid.blade.php
@@ -540,7 +540,7 @@ return fasle;
                                 @endif
                                 <div class="container">
                                     <div class="row">
-                                        <div class="header-navigation-container col-md-12 sachin">
+                                        <div class="header-navigation-container col-md-12">
                                             <div class="row">
                                                 <div class="col-md-12 col-sm-12">
                                                     @include('layouts/elliot/ai_navigation')
