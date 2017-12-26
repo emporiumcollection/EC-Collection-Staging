58743bef3357aaaa3e70ef2e1873c87c00cea8ba
harman <harman@number7even.de>
2017-12-26 10:52:20 +0530

    Footer changes

--- resources/views/pages/editorial.blade.php
+++ resources/views/pages/editorial.blade.php
@@ -513,7 +513,7 @@
                                 <div class="row">
                                     <div class="disply-left-right-align">
                                         <div class="col-md-6 disply-left-right-inner">
-                                            <div class="hotel-block-two-left-sec-align">
+                                            <div class="hotel-block-two-left-sec-align last">
                                                 <h2>{{$propertyDetail['data']->detail_section1_title}}</h2>
                                                 <p>
                                                     {!! nl2br($propertyDetail['data']->detail_section1_description_box1) !!}
