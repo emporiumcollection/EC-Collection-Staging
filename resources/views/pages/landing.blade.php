a658cdc770a3a74d8be862dcad73fc7c2841514e
harman <harman@number7even.de>
2017-12-28 12:28:47 +0530

    sidebar

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -910,10 +910,10 @@
                                     <div id="block-system-main-menu" class="block block-system block-menu no-title odd block-count-6 block-region-sidebar-first block-main-menu" role="navigation">
                                         <div class="block-inner clearfix">
                                             <div class="block-content content" class="block-content content">
-                                                <ul class="menuSeats clearfix">
+                                                <ul class="menu clearfix">
                                                     <li class="first expanded menu-depth-1 menu-item-989">
                                                         <a href="#">Sleep</a>
-                                                        <ul class="menuSubSeats clearfix">
+                                                        <ul class="menu clearfix">
                                                             <li class="first leaf menu-depth-2 menu-item-990">
                                                                 <a href="#">Suite Présidentielle</a>
                                                             </li>
@@ -942,7 +942,7 @@
                                                     </li>
                                                     <li class="expanded menu-depth-1 menu-item-996">
                                                         <a href="#">Savour</a>
-                                                        <ul class="menuSubSeats clearfix">
+                                                        <ul class="menu clearfix">
                                                     <li class="first leaf menu-depth-2 menu-item-998">
                                                         <a href="#">Le Roof</a>
                                                     </li><li class="leaf menu-depth-2 menu-item-999">
@@ -955,7 +955,7 @@
                                                     </li>
                                                     <li class="expanded menu-depth-1 menu-item-1000">
                                                         <a href="#">Relax</a>
-                                                        <ul class="menuSubSeats clearfix">
+                                                        <ul class="menu clearfix">
                                                             <li class="first leaf menu-depth-2 menu-item-1001">
                                                                 <a href="#">Spa</a>
                                                             </li><li class="leaf menu-depth-2 menu-item-1002">
@@ -968,7 +968,7 @@
                                                     </li>
                                                     <li class="expanded menu-depth-1 menu-item-1004">
                                                         <a href="#">Discover</a>
-                                                        <ul class="menuSubSeats clearfix">
+                                                        <ul class="menu clearfix">
                                                             <li class="first leaf menu-depth-2 menu-item-1005">
                                                                 <a href="#">Cannes</a></li>
                                                             <li class="last leaf menu-depth-2 menu-item-1006">
@@ -981,7 +981,7 @@
                                                     </li>
                                                     <li class="expanded menu-depth-1 menu-item-1123">
                                                         <a href="#">Offres</a>
-                                                        <ul class="menuSubSeats clearfix">
+                                                        <ul class="menu clearfix">
                                                             <li class="first leaf menu-depth-2 menu-item-1014">
                                                                 <a href="#">Offres Sleep</a>
                                                             </li><li class="last leaf menu-depth-2 menu-item-1125">
