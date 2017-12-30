a03aedb5be9b0382660ee25b6ab7efee2aeb08e0
harman <harman@number7even.de>
2017-12-30 16:08:44 +0530

    sidebar changing layout

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -13,7 +13,7 @@
         <link href="{{ asset('sximo/assets/css/style-ai.css')}}" rel="stylesheet" type="text/css"/>
         <link href="{{ asset('sximo/assets/css/homepage41e7.css')}}" rel="stylesheet" type="text/css"/>
         
-        <!--<script src="{{ asset('sximo/assets/js/jquery-2.1.0.min.js')}}" type="text/javascript"></script>-->
+        <script src="{{ asset('sximo/assets/js/jquery-2.1.0.min.js')}}" type="text/javascript"></script>
         <script src="{{ asset('sximo/assets/js/bootstrap.min.js')}}" type="text/javascript"></script>
         <script src="{{ asset('sximo/assets/js/jquery-ui.js')}}" type="text/javascript"></script>
         <script src="{{ asset('sximo/assets/js/moment.min.js')}}" type="text/javascript" ></script>
