f967af04170482e773c6b9d7645d5bd4b1260622
harman <harman@number7even.de>
2017-12-30 11:16:00 +0530

    sidebar changing layout

--- resources/views/pages/filters-grid.blade.php
+++ resources/views/pages/filters-grid.blade.php
@@ -382,6 +382,70 @@ return fasle;
                 float: right;
             }
             
+            /* AIC Harman email sidebar css */
+            
+        .site-aside {
+            position: fixed;
+            top: 0;
+            right: 0;
+            height: 100%;
+            overflow-x: visible;
+            z-index: 1028;
+    }
+        .contact-aside {
+            border-radius: 2px;
+            transition: .5s;
+            position: fixed;
+            top: 63px;
+            left: calc(100% - 43px);
+            width: 291px;
+            background: #272727;
+            color: #fff;
+            font-size: 15px;
+            right: 0;
+        }
+        .contact-aside ul {
+            padding: 7px 0;
+            list-style: none;
+        }
+        .contact-aside li {
+            padding: 9.33333px 0;
+            margin: 0 15px;
+        }
+        .contact-aside {
+            color: #fff;
+            font-size: 15px;
+        }
+        .contact-aside [class*="icon-"] {
+            display: inline-block;
+            width: 24px;
+            text-align: center;
+            margin-right: 4px;
+            float: right;
+        }
+        .contact-aside li + li {
+            border-top: 1px solid #4A4A4A;
+        }
+        .contact-aside .icon-mail {
+            position: relative;
+            top: 2px;
+        }
+        .contact-aside.active {
+            left: calc(100% - 290px);
+            background: #1e2023 !important;
+            color: #fff !important;
+        }
+        
+        .asideIclass {
+            float: left;
+            margin-top: 3px;
+            margin-right: 10px;
+            margin-left: 5px;
+        }
+        
+         /* sidebar css */
+            
+            
         </style>
 		
 		<!-- Global site tag (gtag.js) - Google Analytics -->
@@ -2115,6 +2179,14 @@ return fasle;
                             });
                         }));
                     </script>
+                <!-- contact email aside -->
+                <script> 
+                    $('.contact-aside').hover(
+                        function(){ $(this).addClass('active') },
+                        function(){ $(this).removeClass('active') }
+                    )
+                </script>
+                    
                     <link href="{{ asset('sximo/assets/css/editorial.css')}}" rel="stylesheet" type="text/css"/>
                     <div class="editorial-custom-footer-style grid-page-footer-align">
                         @include('layouts/elliot/ai_footer_social')    
