28f2ae629d8fd418b4c06fef9d9c1f13744fb254
Manu Mahesh <manu@number7even.de>
2018-01-02 16:35:50 +0530

    Landing page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -696,48 +696,7 @@ class HomeController extends Controller {
                                                 $landinggridpropertiesArr[$pr]['image'] = $fileArr;
                                                 $landinggridpropertiesArr[$pr]['image']->imgsrc = (new ContainerController)->getThumbpath($fileArr->folder_id);
                                         }
-                                        
-                                        $cat_types = \DB::table('tb_properties_category_types')->select('id','category_name','room_desc')->where('property_id', $ldprop->id)->where('status', 0)->where('show_on_booking', 1)->get();
-                                        if (!empty($cat_types)) {
-                                            $c = 0;
-                                            foreach ($cat_types as $type) {
-                                                
-                                                $roomfileArr = \DB::table('tb_properties_images')->join('tb_container_files', 'tb_container_files.id', '=', 'tb_properties_images.file_id')->select('tb_container_files.file_name', 'tb_container_files.file_size', 'tb_container_files.file_type', 'tb_container_files.folder_id')->where('tb_properties_images.property_id', $ldprop->id)->where('tb_properties_images.category_id', $type->id)->where('tb_properties_images.type', 'Rooms Images')->orderBy('tb_container_files.file_sort_num', 'asc')->get();
-
-                                                $filen = array();
-                                                if (!empty($roomfileArr)) {
-                                                    $landinggridpropertiesArr[$pr]['roomimgs'][$type->id]['imgs'] = $roomfileArr;
-                                                    $landinggridpropertiesArr[$pr]['roomimgs'][$type->id]['imgsrc'] = (new ContainerController)->getThumbpath($roomfileArr[0]->folder_id);
-                                                    $landinggridpropertiesArr[$pr]['roomimgs'][$type->id]['imgsrc_dir'] = public_path(str_replace(url().'/', '', (new ContainerController)->getThumbpath($roomfileArr[0]->folder_id)));
-                                                    $landinggridpropertiesArr[$pr]['typedata'][$c] = $type;
-                                                    $landinggridpropertiesArr[$pr]['typedata'][$c]->price = '';
-                                                    $curnDate = date('Y-m-d');
-                                                    if ($ldprop->default_seasons != 1) {
-                                                        $checkseason = \DB::table('tb_properties_category_rooms_price')->join('tb_seasons','tb_seasons.id','=','tb_properties_category_rooms_price.season_id')->join('tb_seasons_dates','tb_seasons_dates.season_id','=','tb_seasons.id')->select('tb_properties_category_rooms_price.rack_rate')->where('tb_properties_category_rooms_price.property_id', $ldprop->id)->where('tb_properties_category_rooms_price.category_id', $type->id)->where('tb_seasons.property_id', $ldprop->id)->where('tb_seasons_dates.season_from_date', '>=', $curnDate)->where('tb_seasons_dates.season_to_date', '<=', $curnDate)->orderBy('tb_seasons.season_priority', 'asc')->first();
-                                                    } else {
-                                                        $checkseason = \DB::table('tb_properties_category_rooms_price')->join('tb_seasons','tb_seasons.id','=','tb_properties_category_rooms_price.season_id')->join('tb_seasons_dates','tb_seasons_dates.season_id','=','tb_seasons.id')->select('tb_properties_category_rooms_price.rack_rate')->where('tb_properties_category_rooms_price.property_id', $ldprop->id)->where('tb_properties_category_rooms_price.category_id', $type->id)->where('tb_seasons.property_id', 0)->where('tb_seasons_dates.season_from_date', '>=', $curnDate)->where('tb_seasons_dates.season_to_date', '<=', $curnDate)->first();
-                                                    }
-
-                                                    if (!empty($checkseason)) {
-                                                        $landinggridpropertiesArr[$pr]['typedata'][$c]->price = $checkseason->rack_rate;
-                                                    } else {
-                                                        $checkseasonPrice_ifnotanyseason = \DB::table('tb_properties_category_rooms_price')->select('rack_rate')->where('season_id', 0)->where('property_id', $ldprop->id)->where('category_id', $type->id)->first();
-                                                        if (!empty($checkseasonPrice_ifnotanyseason)) {
-                                                            $landinggridpropertiesArr[$pr]['typedata'][$c]->price = $checkseasonPrice_ifnotanyseason->rack_rate;
-                                                        }
-                                                    }
-                                                    $c++;
-                                                }
-                                                
-                                                /*$roomfileArr = \DB::table('tb_properties_images')->select('id')->where('property_id', $ldprop->id)->where('category_id', $type->id)->where('type', 'Rooms Images')->count();
-                                                if ($roomfileArr>0) {
-                                                    $landinggridpropertiesArr[$pr]['typedata'][$c] = $type;
-                                                    $landinggridpropertiesArr[$pr]['roomimgs'][$type->id] = 'yes';
-                                                    $c++;
-                                                }*/
-                                            } 
-                                        }
-                                        
+                                                                                
                                         $pr++;
                                 }
                         }
