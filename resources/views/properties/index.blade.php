fe7f0bdf47d6651b9b2d29a998185cd4d7754d7d
puneetsachdeva-number7even <puneet@number7even.de>
2018-01-02 14:44:35 +0530

    revert change

--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -86,8 +86,8 @@
 					<td> <a target="_blank" href="{{URL::to('search?s='.$row->city)}}">{{$row->city}}</a> </td>
 					<td> <a target="_blank" href="{{(strpos($row->website, 'http') !== false) ? $row->website : 'http://'.$row->website }}">{{$row->website}}</a> </td>
 					<td> {{$row->email}} </td>
-				 <td>	{{$row->assign_detail_city}}
-						@if($row->assign_detail_city!=0 && $row->assign_detail_city!='')
+				 <td>
+						@if($row->assign_detail_city!='0' && $row->assign_detail_city!='')
 							<a href="#" class="tips btn btn-xs btn-primary" title="City Assigned"><i class="fa  fa-check "></i></a>
 						@else
 							<a href="#" class="tips btn btn-xs btn-danger" title="please assign city"><i class="fa  fa-times "></i></a>
