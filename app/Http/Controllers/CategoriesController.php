0495c33254710e52c831598416ba971b5eb54d66
puneetsachdeva-number7even <puneet@number7even.de>
2018-01-02 18:48:21 +0530

    test function

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -97,6 +97,7 @@ class CategoriesController extends Controller {
 		
 		if($request->input('ctupdate') == 'yes')
 		{
+			die;
 			foreach($results['rows'] as $catup)
 			{
 				$udata['category_custom_title'] = 'Luxury Destination '.$catup->category_name;
