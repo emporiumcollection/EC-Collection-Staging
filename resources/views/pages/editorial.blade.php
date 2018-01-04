a698fde83c63c3f7637fa82c809ecd2352661b7c
harman <harman@number7even.de>
2018-01-04 10:38:07 +0530

    detail page changes

--- resources/views/pages/editorial.blade.php
+++ resources/views/pages/editorial.blade.php
@@ -147,6 +147,12 @@
                 margin-top: 22px;
                 font-family: Geomanist-Light;
             }
+            .hotels-image-showcase-des-text {
+                /* height: 392px; */
+                float: none;
+                top: 255px;
+                position: absolute;
+            }
             .hotel-block-two-right-sec-align > h2{
                 color: #dbd4d7;
                 font-family: ACaslonPro-Regular;
@@ -572,7 +578,7 @@
                                 font-family: Raleway-ExtraLight;
                                 font-size: 70px;
                                 margin-left: -107px;
-                                margin-top: 36px;
+                                margin-top: 550px;
                                 margin-top: 0px;
                                 text-transform: uppercase;
                             }
