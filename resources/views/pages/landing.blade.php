4dda5609be2d147cb892bf0315d56e550cb26773
harman <harman@number7even.de>
2018-01-05 17:45:48 +0530

    hamburg menu remove

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -2317,7 +2317,7 @@
                                      <div class="ps-login-signup-form-top-bar1">
                                         <div class="col-md-12 col-sm-6">
                                              <div class="row">
-                                                 <div class="ps-forms-cross-icons1">
+                                                 <div class="ps-forms-cross-icons">
                                                      <a class="show-account-with-us  ps-forms-small-heading-link" href="javascript:void(0)">&times;</a>
                                                  </div>
                                              </div>
