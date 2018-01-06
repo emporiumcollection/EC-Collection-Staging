02c50247fb81ae610577f5dbce4459e45c576d97
riaankleynhans <riaan@number7even.com>
2018-01-06 16:57:53 +0530

    Update installcomposer.sh

--- scripts/installcomposer.sh
+++ scripts/installcomposer.sh
@@ -9,6 +9,7 @@ composer update -d /var/www/vhosts/emporium-voyage.com/public_html
 chown -R www-data:www-data /var/www/vhosts/emporium-voyage.com
 sudo find /var/www/vhosts/emporium-voyage.com/public_html -type d -exec chmod 755 {} +
 sudo find /var/www/vhosts/emporium-voyage.com/public_html -type f -exec chmod 755 {} +
+cp -R /var/www/vhosts/emporium-voyage.com/storage /var/www/vhosts/emporium-voyage.com/public_html/storage
 chmod -R 777 /var/www/vhosts/emporium-voyage.com/public_html/storage
 mkdir /var/www/vhosts/emporium-voyage.com/public_html/public/uploads
 s3fs emporium-voyage-live /var/www/vhosts/emporium-voyage.com/public_html/public/uploads/ -o passwd_file=/etc/passwd-s3fs  -ouid=33,gid=33,allow_other,mp_umask=002
