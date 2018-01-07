cb8b18d7e42f83101ad7b4c919e15c8873b5514e
riaankleynhans <riaan@number7even.com>
2018-01-07 10:26:19 +0530

    Update beforeinstall.sh

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo umount -l s3fs
+sudo cp -R /var/www/vhosts/emporium-voyage.com/public_html/storage/* /var/www/vhosts/emporium-voyage.com/storage
 sudo rm -rf /var/www/vhosts/emporium-voyage.com/public_html
