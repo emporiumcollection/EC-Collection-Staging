005aea8cb748dd6cc3281c4184c319cf41675cd3
Harman <harman@number7even.de>
2018-01-09 19:36:40 +0530

    font changes

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -2043,7 +2043,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <!--<div class="panel panel-default  sub-menues">
+                                <div class="panel panel-default  sub-menues">
                                     <a class="collapsed" data-toggle="collapse" data-parent="#accordion-mobile" href="#collapse-mobile-4">
                                         <div class="panel-heading">
                                             <h4 class="panel-title menu-text accordion">
@@ -2096,7 +2096,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div>-->
+                                </div>
                             </div>
                             <!--Footer Section-->
                         </div>
