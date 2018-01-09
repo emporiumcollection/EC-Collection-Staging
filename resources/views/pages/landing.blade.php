94d95b02d9107db23d8b239c46343fd5bec5be79
Harman <harman@number7even.de>
2018-01-09 19:46:52 +0530

    font changes

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -1972,7 +1972,7 @@
                                     </a>
                                 </div>-->
                                 <div class="panel panel-default  sub-menues">
-                                    <a href="#" class="trigger-click-on-book-now collapsed">
+                                    <a href="#collapse-mobile-4" class="trigger-click-on-book-now collapsed" data-toggle="collapse" data-parent="#accordion-mobile">
                                         <div class="panel-heading">
                                             <h4 class="panel-title menu-text accordion">
                                                 Search By Date
