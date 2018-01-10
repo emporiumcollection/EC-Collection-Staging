85a58091bb5e4ee446aecbe780cd9b5f48297e56
Harman <harman@number7even.de>
2018-01-10 11:05:23 +0530

    detail page popup

--- resources/views/pages/editorial.blade.php
+++ resources/views/pages/editorial.blade.php
@@ -29,6 +29,9 @@
         <link href="{{ asset('sximo/assets/css/detail-page.css')}}" rel="stylesheet" type="text/css"/>
         <link href="{{ asset('sximo/assets/css/stick-nav-arrows.css')}}" rel="stylesheet" type="text/css"/>
 		 <link href="{{ asset('sximo/assets/css/slick.css')}}" rel="stylesheet" type="text/css"/>
+                 <!-- AIC 10-1-2017-->
+                 <link href="{{'asset('sximo/assets/css/style.min.css')}}" rel="stylesheet" type="text/css"/> 
+                 <!-- AIC 10-1-2017-->
         <link href="{{ asset('sximo/assets/css/slick-theme.css')}}" rel="stylesheet" type="text/css"/>
         <!--JS Includes-->
         <link href="{{ asset('sximo/assets/css/ai_yachts-custom.css')}}" rel="stylesheet" type="text/css"/>
