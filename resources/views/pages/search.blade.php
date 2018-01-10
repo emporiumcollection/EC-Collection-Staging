254c80d78737632a33b73238bc921f31a04bff35
Ravinder Singh <ravinder@number7even.de>
2018-01-10 21:08:32 +0530

    updated data variables

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -1276,50 +1276,50 @@
                                                 <div class="wrapperforliineedforlightboxremoval">
                                                     <div class="cat_product_medium1">
                                                         <div class="ai-grid-page-node-pic-box pictureBox gridPicture grid-box-main">
-                                                            @if($props['data']->price!='')
-                                                            <?php /*<a class="textButton arrowButton MrgTop5 ai-filter-hotel-price-style" rel="{{$props['data']->id}}" href="{{URL::to($props['data']->property_slug)}}">
-                                                                From EUR {{$props['data']->price}} / night 
+                                                            @if($props->price!='')
+                                                            <?php /*<a class="textButton arrowButton MrgTop5 ai-filter-hotel-price-style" rel="{{$props->id}}" href="{{URL::to($props->property_slug)}}">
+                                                                From EUR {{$props->price}} / night 
                                                             </a> */ ?>
                                                             @endif
                                                             @if(array_key_exists('image', $props))
-                                                                @if($props['data']->editor_choice_property=='1')
+                                                                @if($props->editor_choice_property=='1')
                                                                     <img alt="editor_choice_property" class="propovericons" src="{{URL::to('sximo/images/editors-choice.png')}}">
-                                                                @elseif($props['data']->feature_property=='1')
+                                                                @elseif($props->feature_property=='1')
                                                                     <img alt="editor_choice_property" class="propovericons" src="{{URL::to('sximo/images/featured-property.png')}}">
                                                                 @endif
                
-                                                                <a title="{{$props['data']->property_name}}" class="picture_link-" href="{{URL::to($props['data']->property_slug)}}" rel="{{$props['data']->id}}">
+                                                                <a title="{{$props->property_name}}" class="picture_link-" href="{{URL::to($props->property_slug)}}" rel="{{$props->id}}">
                                                                     <div class="overlay-text-frezeed">
-                                                                        <h2 class="yacts-tittle-text">{{ $props['data']->property_name}}</h2>
-                                                                        <p class="yacths-des-text yacths-des-text-align"><span>&euro;<?php echo $props['data']->price; ?> </span>|<span>37.7mm</span>|<span>10 Guests</span></p>
+                                                                        <h2 class="yacts-tittle-text">{{ $props->property_name}}</h2>
+                                                                        <p class="yacths-des-text yacths-des-text-align"><span>&euro;<?php echo $props->price; ?> </span>|<span>37.7mm</span>|<span>10 Guests</span></p>
                                                                     </div>
                                                                     <div class="overlay-text hotel-overlay-text">
-                                                                        <h2 class="yacts-tittle-text">{{ $props['data']->property_name}}</h2>
-                                                                        <p class="yacths-des-text yacths-des-text-align"><span>From &euro;{{ $props['data']->price}} </span>
-                                                                        <span>      {{(isset($props['data']->category_name))? '| '.$props['data']->category_name : ''}}</span></p>
+                                                                        <h2 class="yacts-tittle-text">{{ $props->property_name}}</h2>
+                                                                        <p class="yacths-des-text yacths-des-text-align"><span>From &euro;{{ $props->price}} </span>
+                                                                        <span>      {{(isset($props->category_name))? '| '.$props->category_name : ''}}</span></p>
                                                                     </div>
                                                                     <div class="overlay-text yacts-overlay-text">
-                                                                        <h2 class="yacts-tittle-text">{{ $props['data']->property_name }}</h2>
-                                                                        <p class="yacths-des-text yacths-des-text-align"><span>&euro;{{ $props['data']->price}} </span>|<span>37.7mm</span>|<span>10 Guests</span></p>
+                                                                        <h2 class="yacts-tittle-text">{{ $props->property_name }}</h2>
+                                                                        <p class="yacths-des-text yacths-des-text-align"><span>&euro;{{ $props->price}} </span>|<span>37.7mm</span>|<span>10 Guests</span></p>
                                                                         <p class="yacths-des-text">2015H</p>
                                                                     </div>
                                                                     <img alt="{{ $props['image']->file_name }}" src="{{ URL::to('uploads/property_imgs_thumbs/front_property_'.$props['image']->folder_id.'_'.$props['image']->file_name)}}" class="img-responsive">
                                                                 </a>
                                                             @else
-                                                            <a class="picture_link detail_view-" rel="{{$props['data']->id}}" href="{{URL::to($props['data']->property_slug)}}">
+                                                            <a class="picture_link detail_view-" rel="{{$props->id}}" href="{{URL::to($props->property_slug)}}">
                                                                 <img class="img-responsive" src="{{URL::to('sximo/assets/images/img-1.jpg')}}" alt="">
                                                             </a>
 
                                                             <div class="overlay-text-frezeed">
-                                                                <h2 class="yacts-tittle-text">{{ $props['data']->property_name}}</h2>
+                                                                <h2 class="yacts-tittle-text">{{ $props->property_name}}</h2>
                                                                 <p class="yacths-des-text yacths-des-text-align"><span>&euro;500 </span>|<span>37.7mm</span>|<span>10 Guests</span></p>
                                                             </div>
                                                             <div class="overlay-text hotel-overlay-text">
-                                                                <h2 class="yacts-tittle-text">{{ $props['data']->property_name }}</h2>
-                                                                <p class="yacths-des-text yacths-des-text-align"><span>From &euro;{{ $props['data']->price}} </span>|<span>New York</span></p>
+                                                                <h2 class="yacts-tittle-text">{{ $props->property_name }}</h2>
+                                                                <p class="yacths-des-text yacths-des-text-align"><span>From &euro;{{ $props->price}} </span>|<span>New York</span></p>
                                                             </div>
                                                             <div class="overlay-text yacts-overlay-text">
-                                                                <h2 class="yacts-tittle-text">{{ $props['data']->property_name }}</h2>
+                                                                <h2 class="yacts-tittle-text">{{ $props->property_name }}</h2>
                                                                 <p class="yacths-des-text yacths-des-text-align"><span>&euro;500 </span>|<span>37.7mm</span>|<span>10 Guests</span></p>
                                                                 <p class="yacths-des-text">2015H</p>
                                                             </div>
@@ -1330,24 +1330,24 @@
                                                         <div class="listDetails">
                                                             <div class="photographBox ai-grid-tiitles">
                                                                 <h2>
-                                                                    <a title="{{$props['data']->property_name}}" class="photograph FltLft ai-filtreted-hotel-name" rel="{{$props['data']->id}}" href="{{URL::to($props['data']->property_slug)}}">
-                                                                        {{$props['data']->property_name}}
+                                                                    <a title="{{$props->property_name}}" class="photograph FltLft ai-filtreted-hotel-name" rel="{{$props->id}}" href="{{URL::to($props->property_slug)}}">
+                                                                        {{$props->property_name}}
                                                                     </a>
                                                                     <span class="FltRgt">
-                                                                        <!--i class="fa fa-camera-retro colorGrey" aria-hidden="true" title="Add to Itinerary" @if(array_key_exists('image', $props)) onclick="add_to_lightbox({{$props['image']->file_id}}, {{$props['data']->id}});" @endif ></i-->
+                                                                        <!--i class="fa fa-camera-retro colorGrey" aria-hidden="true" title="Add to Itinerary" @if(array_key_exists('image', $props)) onclick="add_to_lightbox({{$props['image']->file_id}}, {{$props->id}});" @endif ></i-->
 
-                                                                       <a class="carticon" href="javascript:void(0)" onclick="submitgridbookform('{{$props['data']->property_slug}}#*{{$props['data']->id}}');"><i class="fa fa-shopping-cart colorGrey" aria-hidden="true" title="book this hotel"></i></a>
+                                                                       <a class="carticon" href="javascript:void(0)" onclick="submitgridbookform('{{$props->property_slug}}#*{{$props->id}}');"><i class="fa fa-shopping-cart colorGrey" aria-hidden="true" title="book this hotel"></i></a>
                                                                     </span>
                                                                 </h2>
 
                                                             </div>
 
                                                             <div class="entire_story MrgTop5 ai-view-hotels-tittle">
-                                                                <a class="textButton arrowButton detail_view MrgTop5" rel="{{$props['data']->id}}" href="#">
+                                                                <a class="textButton arrowButton detail_view MrgTop5" rel="{{$props->id}}" href="#">
                                                                     Quick View 
                                                                 </a>
 
-                                                                <a class="textButton arrowButton MrgTop5" rel="{{$props['data']->id}}" href="{{URL::to($props['data']->property_slug)}}">
+                                                                <a class="textButton arrowButton MrgTop5" rel="{{$props->id}}" href="{{URL::to($props->property_slug)}}">
                                                                     Detail View 
                                                                 </a>
                                                             </div>
