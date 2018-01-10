d762408634b824d560ee01342218414ef48383d2
Ravinder Singh <ravinder@number7even.de>
2018-01-10 21:26:33 +0530

    updated

--- app/Helpers/CustomQuery.php
+++ app/Helpers/CustomQuery.php
@@ -4,8 +4,8 @@ namespace App\Helpers;
 class CustomQuery
 {
     //Return All images path of Property
-    static function getPropertyImages($prop){
-		$propertyFile = 'property/'.$prop->id.'.json'; 
+    static function getPropertyImages($propId){
+		$propertyFile = 'property/'.$propId.'.json'; 
 		$contents = Storage::get($propertyFile);
 		$proertyObj = json_decode($contents);
 		return $proertyObj;
