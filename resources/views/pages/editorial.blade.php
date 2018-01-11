3761fbd9684984604edcb470967bc6a0af6003b6
Harman <harman@number7even.de>
2018-01-11 18:29:04 +0530

    search page popup

--- resources/views/pages/editorial.blade.php
+++ resources/views/pages/editorial.blade.php
@@ -2458,15 +2458,16 @@
         <!--Gallery popup end-->
         
          <!-- my popup hamburg menu -->
-        <div id="register-popup" class="popup personlized-service-pop-up-outer">
-            <div class="popup-inner personlized-service-pop-up">
+        <!-- my popup hamburg menu -->
+        <div id="ev-primary-navigation" class="popup personlized-service-pop-up-outer">
+            <div class="popup-inner personlized-service-pop-up EVpopService">
                 <a href="#" class="popup-close-btn personlized-service-pop-up-close-btn">&times;</a>
                 <div class="popup-content personlized-service-content EVhamburgService">
                     <!-- code here-->
                     <div class="col-md-12">
-			<div id="ev-primary-navigation" class="popup personlized-service-pop-up-outer">
+			<div id="" class="">
 				<div class="popup-inner personlized-service-pop-up EVpopService">
-					<a href="#" class="popup-close-btn personlized-service-pop-up-close-btn">&times;</a>
+					<!--<a href="#" class="popup-close-btn personlized-service-pop-up-close-btn">&times;</a>-->
 					<div class="popup-content personlized-service-content">
 						<div class="popup-form-center EVcenterPOP">
 							<ul class="EVMenu">
@@ -2535,7 +2536,7 @@
                 </div>
             </div>
         </div>
-        <!-- my popup hamburg menu -->
+        <!--my popup hamburg menu End Here-->
         
 		
         <script>
