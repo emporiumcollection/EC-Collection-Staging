8c71c7b007e25142063272fcdaea6bcbdb96f599
Harman <harman@number7even.de>
2018-01-11 14:30:07 +0530

    landing page close icon

--- resources/views/pages/editorial.blade.php
+++ resources/views/pages/editorial.blade.php
@@ -2144,28 +2144,113 @@
             @endif
         </div>
         <!--Featured hotel popup end-->
-	<!--New Login Pop Up Start Here-->
+	 <!--Register Pop Up Start Here main-->
+        <div id="register-popup" class="popup personlized-service-pop-up-outer">
+            <div class="popup-inner personlized-service-pop-up">
+                <a href="#" class="popup-close-btn personlized-service-pop-up-close-btn">&times;</a>
+                <div class="popup-content personlized-service-content">
+                    <div class="popup-form-center">
+                        <div class="form-tittle">
+                            <h3>JohnnyShares - Sharing & Collaboration Platform</h3>
+                        </div> 
+                        <div class="form-content clearfix">
+                            <div class="form-logo">
+                                <img class="img-responsive" src="{{ asset('sximo/assets/images/logo-design_1.png')}}" alt="Design Locations">
+                            </div>
+                            <div>
+                                <ul class="navigation-tabs">
+                                    <li><a href="#" class="active" id="sign-in-form-link">Sign in</a></li>
+                                    <li><a href="#" id="forgot-password-link">Forgot Password</a></li>
+                                    <li><a href="#" id="register-form-link">Register</a></li>
+                                </ul>
+                            </div>
+                            <div class="input-fileds">
+                                <form  id="login-form" role="form" style="display: none;">
+                                    <div class="form-group">
+                                        <label>Email Address</label>
+                                        <input type="text" name="email_address" placeholder="Email Address" class="form-control">
+                                        <i class="fa fa-user input-fa" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <input placeholder="Password" type="Password" name="password"  class="form-control">
+                                        <i class="fa fa-lock input-fa" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="form-group">
+                                        <label> Remember Me ? </label>
+                                        <input class="remember-me-checkbox" name="remember" value="1" type="checkbox">
+                                        <i class="fa fa-lock input-fa remember-me-fa" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Language</label>
+                                        <select class="form-control" name="language">
+                                            <option value="Deutsch"> Deutsch</option>
+                                            <option value="en"> English</option>
+                                        </select>
+                                    </div>
+                                    <button class="btn btn-info form-sign-in-btn btn-sm btn-block" type="submit">Sign In</button>
+                                </form>
+                                <form id="register-form" role="form" style="display: block;">
+                                    <div class="form-group">
+                                        <label>Name</label>
+                                        <input type="text" name="user_name" placeholder="Name" class="form-control">
+                                        <i class="fa fa-user input-fa" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Email Address</label>
+                                        <input type="text" name="email_address" placeholder="Email Address" class="form-control">
+                                        <i class="fa fa-user input-fa" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <input placeholder="Password" type="Password" name="password"  class="form-control">
+                                        <i class="fa fa-lock input-fa" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Confirm Password</label>
+                                        <input placeholder="Confirm Password" type="Password" name="password"  class="form-control">
+                                        <i class="fa fa-lock input-fa" aria-hidden="true"></i>
+                                    </div>
+                                    <button class="btn btn-info form-sign-in-btn btn-sm btn-block" type="submit">Sign Up</button>
+                                </form>
+                                <form id="forgot-password" role="form" style="display: none;">
+                                    <div class="form-group">
+                                        <label>Email Address</label>
+                                        <input type="text" name="email_address" placeholder="Email Address" class="form-control">
+                                        <i class="fa fa-user input-fa" aria-hidden="true"></i>
+                                    </div>
+                                    <button class="btn btn-info form-sign-in-btn btn-sm btn-block" type="submit">Recover Account</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+        <!--Register Pop Up End Here main-->
+        <!--New Login Pop Up Start Here main-->
         <div id="login-forms-popup" class="popup login-form-pop-main-align">
              <div class="popup-inner">
                  <a href="#" class="popup-close-btn">&times;</a>
                  <div class="popup-content">
                      <div class="content-area">
-                         <a class="dl-pop-logo-align" href="#"><img class="img-responsive" src="{{ asset('sximo/assets/images/design-location-logo.png')}}"></a>
+<!--                         <a class="dl-pop-logo-align" href="#"><img class="img-responsive" src="{{ asset('sximo/assets/images/design-location-logo.png')}}"></a>-->
                      </div>
                      <!--Login Forms Start Here-->
                      <div class="landing-page-lock-login-btn-outer-align">
-                         <div class="login-form-show-hide">
+                         <div class="login-form-show-hide" style="display:block;">
                              <div class="login-sign-up-sidebar-outer-align">
                                  <div class="your-account-heading-align">
                                      <div class="ps-login-signup-form-top-bar">
-                                         <div class="col-md-6 col-sm-6">
+<!--                                         <div class="col-md-6 col-sm-6">
                                              <div class="row">
                                                  <div class="ps-forms-cross-icons">
                                                      <a class="show-account-with-us ps-forms-small-heading-link" href="javascript:void(0)">&times;</a>
                                                  </div>
                                              </div>
-                                         </div>
-                                         <div class="col-md-6 col-sm-6">
+                                         </div>-->
+                                         <div class="col-md-12 col-sm-6">
                                              <div class="row">
                                                  <div class="right-need-help-icon">
                                                      <a class="ps-forms-small-heading-link" href="#">Need Help?</a>
@@ -2173,6 +2258,15 @@
                                              </div>
                                          </div>
                                      </div>
+                                     <div class="ps-login-signup-form-top-bar1">
+                                        <div class="col-md-12 col-sm-6">
+                                           <div class="row">
+                                               <div class="ps-forms-cross-icons">
+                                                   <a class="show-account-with-us ps-forms-small-heading-link" href="javascript:void(0)">&times;</a>
+                                               </div>
+                                           </div>
+                                       </div>
+                                    </div>
                                      <div class="clearfix"></div>
                                      <div class="ps-form-main-pannel">
                                          <div class="ps-form-heading-outer-align">
@@ -2203,14 +2297,14 @@
                              <div class="login-sign-up-sidebar-outer-align">
                                  <div class="your-account-heading-align">
                                      <div class="ps-login-signup-form-top-bar">
-                                         <div class="col-md-6 col-sm-6">
+<!--                                         <div class="col-md-6 col-sm-6">
                                              <div class="row">
                                                  <div class="ps-forms-cross-icons">
                                                      <a class="show-account-with-us ps-forms-small-heading-link" href="javascript:void(0)">&times;</a>
                                                  </div>
                                              </div>
-                                         </div>
-                                         <div class="col-md-6 col-sm-6">
+                                         </div>-->
+                                         <div class="col-md-12 col-sm-6">
                                              <div class="row">
                                                  <div class="right-need-help-icon">
                                                      <a class="ps-forms-small-heading-link" href="#">Need Help?</a>
@@ -2218,6 +2312,15 @@
                                              </div>
                                          </div>
                                      </div>
+                                     <div class="ps-login-signup-form-top-bar1">
+                                        <div class="col-md-12 col-sm-6">
+                                           <div class="row">
+                                               <div class="ps-forms-cross-icons">
+                                                   <a class="show-account-with-us ps-forms-small-heading-link" href="javascript:void(0)">&times;</a>
+                                               </div>
+                                           </div>
+                                       </div>
+                                    </div>
                                      <div class="clearfix"></div>
                                      <div class="ps-form-main-pannel">
                                          <div class="ps-form-heading-outer-align">
@@ -2245,17 +2348,26 @@
                              <div class="login-sign-up-sidebar-outer-align">
                                  <div class="your-account-heading-align">
                                      <div class="ps-login-signup-form-top-bar">
-                                         <div class="col-md-6 col-sm-6">
+<!--                                         <div class="col-md-6 col-sm-6">
                                              <div class="row">
-                                                 <div class="ps-forms-cross-icons">
+                                                 <div class="ps-forms-cross-icons1">
                                                      <a class="show-account-with-us  ps-forms-small-heading-link" href="javascript:void(0)">&times;</a>
                                                  </div>
                                              </div>
-                                         </div>
-                                         <div class="col-md-6 col-sm-6">
+                                         </div>-->
+                                         <div class="col-md-12 col-sm-6">
                                              <div class="row">
                                                  <div class="right-need-help-icon">
                                                      <a class="ps-forms-small-heading-link" href="#">Need Help?</a>
+                                                 </div>
+                                             </div>
+                                         </div>
+                                     </div>
+                                     <div class="ps-login-signup-form-top-bar1">
+                                        <div class="col-md-12 col-sm-6">
+                                             <div class="row">
+                                                 <div class="ps-forms-cross-icons">
+                                                     <a class="show-account-with-us  ps-forms-small-heading-link" href="javascript:void(0)">&times;</a>
                                                  </div>
                                              </div>
                                          </div>
@@ -2317,7 +2429,7 @@
                  </div>
              </div>
          </div>
-        <!--New Login Pop Up End Here-->
+        <!--New Login Pop Up End Here main-->
 		<div id="detail-page-gallery-popup" class="popup detail-page-room-pop-up-align">
 			<div class="popup-inner">
 				<a href="javascript:void(0);" class="popup-close-btn">CLOSE</a>
