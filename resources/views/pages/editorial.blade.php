46a54cb85f1e29e584f92fd1673a78140b11e109
Harman <harman@number7even.de>
2018-01-12 17:38:33 +0530

    detail page images change

--- resources/views/pages/editorial.blade.php
+++ resources/views/pages/editorial.blade.php
@@ -2288,7 +2288,7 @@
                                      </div>
                                  </div>
                                  <div class="ps-login-sign-up-image">
-                                     <img class="img-responsive" src="{{ asset('sximo/assets/images/Kootenay Aurora 1-X3.jpg')}}" alt=""/>
+                                     <img class="img-responsive" src="{{ asset('sximo/assets/images/step.jpg')}}" alt=""/>
                                  </div>
                              </div>
                          </div>
@@ -2392,7 +2392,7 @@
                                      </div>
                                  </div>
                                  <div class="ps-login-sign-up-image">
-                                     <img class="img-responsive" src="{{ asset('sximo/assets/images/Step.jpg')}}" alt=""/>
+                                     <img class="img-responsive" src="{{ asset('sximo/assets/images/matthew-kane-365718.jpg')}}" alt=""/>
                                  </div>
                              </div>
                          </div>
