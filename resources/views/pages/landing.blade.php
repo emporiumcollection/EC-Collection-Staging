018b4e1531be0e34a729d5420f3b06d4094824a7
Harman <harman@number7even.de>
2018-01-12 11:06:41 +0530

    landing page changes

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -1874,7 +1874,7 @@
                                     </a>
                                 </div>-->
                                 <div class="panel panel-default  sub-menues">
-                                    <a href="#" class="trigger-click-on-book-now collapsed">
+                                    <a href="#collapse-mobile-4" class="trigger-click-on-book-now collapsed">
                                         <div class="panel-heading">
                                             <h4 class="panel-title menu-text accordion">
                                                 Search By Date
