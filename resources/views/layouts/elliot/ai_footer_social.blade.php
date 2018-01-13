728186440bc704939a400497c846dc55e3730bb6
Harman <harman@number7even.de>
2018-01-13 14:56:16 +0530

    detail page top to bottom

--- resources/views/layouts/elliot/ai_footer_social.blade.php
+++ resources/views/layouts/elliot/ai_footer_social.blade.php
@@ -237,7 +237,7 @@
                 });
             });
         </script>
-        <script>
+<!--        <script>
             $(document).ready(function () {
                 $(window).scroll(function () {
                     if ($(this).scrollTop() > 100) {
@@ -255,7 +255,7 @@
                     $(".open-video-pop-up").trigger("click");
                 });
             });
-        </script>
+        </script>-->
         <script>
             /* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
             var acc = document.getElementsByClassName("accordion");
