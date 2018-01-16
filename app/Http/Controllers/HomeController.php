55a6bd12e6b3f4466ee1a1e0f21a6856a52e1562
Manu Mahesh <manu@number7even.de>
2018-01-16 15:33:45 +0530

    Grid page fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6257,12 +6257,10 @@ class HomeController extends Controller {
                         $query .= ", (SELECT pcrp.rack_rate FROM tb_properties_category_rooms_price pcrp  where pr.id=pcrp.property_id  order by pcrp.rack_rate DESC limit 0,1 ) as price " ;
                         $query .= " FROM tb_properties pr ";
                         $whereClause =" WHERE ((pr.property_name LIKE '%$keyword%'AND pr.property_type = 'Hotel') OR city LIKE '%$keyword%' ".$catprops." ) AND pr.property_status = 1  ";
-                        $orderBy = "ORDER BY (SELECT rack_rate FROM tb_properties_category_rooms_price pcrp, tb_properties pr WHERE pcrp.property_id = pr.id ORDER BY rack_rate DESC LIMIT 1) * 1 DESC, pr.editor_choice_property desc, pr.feature_property desc ";
+                        $orderBy = "ORDER BY (SELECT rack_rate FROM tb_properties_category_rooms_price pcrp WHERE pcrp.property_id = pr.id ORDER BY rack_rate DESC LIMIT 1) * 1 DESC, pr.editor_choice_property desc, pr.feature_property desc ";
                         $limit = " LIMIT ". $pageStart.",".$perPage; 
                         $finalQry = $query.$whereClause.$orderBy.$limit ; 
                         $CountRecordQry = "Select count(*) as total_record from tb_properties pr ".$whereClause ;
-                        
-                        echo $finalQry;die;
                         
                         $property = DB::select($finalQry);
                         $getRec = DB::select($CountRecordQry);
