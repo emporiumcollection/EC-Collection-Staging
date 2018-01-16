0281ccdf8cb6fe905087e8d730692a76350bd901
puneetsachdeva-number7even <puneet@number7even.de>
2018-01-16 15:44:57 +0530

    search suggestion refine

--- app/Helpers/TagsFinder.php
+++ app/Helpers/TagsFinder.php
@@ -8,7 +8,11 @@ class TagsFinder {
         $tagStr = "";
         $tags = \DB::table('tb_tags_manager')->where('tag_status', 1)->get();
         foreach ($tags as $tag) {
-            $tagStr .= "'" . $tag->tag_title . "',";
+			$TagsCon = \DB::table('tb_container_tags')->where('tag_id', $tag->id)->count();
+			if($TagsCon>0)
+			{
+				$tagStr .= "'" . $tag->tag_title . "',";
+			}
         }
         $proprty = \DB::table('tb_properties')->where('property_status', 1)->get();
         foreach ($proprty as $propt) {
@@ -16,7 +20,27 @@ class TagsFinder {
         }
         $categories = \DB::table('tb_categories')->where('category_published', 1)->get();
         foreach ($categories as $cats) {
-            $tagStr .= "'" . $cats->category_name . "',";
+			$subdest = \DB::table('tb_categories')->select('id', 'parent_category_id', 'category_name')->where('parent_category_id', $cats->id)->get();
+			$getcats = '';
+			$chldIds = array();
+			if (!empty($subdest)) {
+				$chldIds = (new HomeController)->fetchcategoryChildListIds($cats->id);
+				array_unshift($chldIds, $cats->id);
+			} else {
+				$chldIds[] = $cats->id;
+			}
+
+			if (!empty($chldIds)) {
+				$getcats = " AND (" . implode(" || ", array_map(function($v) {
+									return sprintf("FIND_IN_SET('%s', property_category_id)", $v);
+								}, array_values($chldIds))) . ")";
+			}
+
+			$preprops = DB::select(DB::raw("SELECT COUNT(*) AS total_rows FROM tb_properties WHERE property_status = '1' $getcats"));
+
+			if (isset($preprops[0]->total_rows) && $preprops[0]->total_rows > 0) {
+				$tagStr .= "'" . $cats->category_name . "',";
+			}
         }
         return substr($tagStr, 0, -1);
     }
@@ -25,7 +49,27 @@ class TagsFinder {
         $destStr = "";
         $categories = \DB::table('tb_categories')->where('category_published', 1)->where('parent_category_id', '!=', 8)->where('id', '!=', 8)->get();
         foreach ($categories as $cats) {
-            $destStr .= "'" . $cats->category_name . "',";
+			$subdest = \DB::table('tb_categories')->select('id', 'parent_category_id', 'category_name')->where('parent_category_id', $cats->id)->get();
+			$getcats = '';
+			$chldIds = array();
+			if (!empty($subdest)) {
+				$chldIds = (new HomeController)->fetchcategoryChildListIds($cats->id);
+				array_unshift($chldIds, $cats->id);
+			} else {
+				$chldIds[] = $cats->id;
+			}
+
+			if (!empty($chldIds)) {
+				$getcats = " AND (" . implode(" || ", array_map(function($v) {
+									return sprintf("FIND_IN_SET('%s', property_category_id)", $v);
+								}, array_values($chldIds))) . ")";
+			}
+
+			$preprops = DB::select(DB::raw("SELECT COUNT(*) AS total_rows FROM tb_properties WHERE property_status = '1' $getcats"));
+
+			if (isset($preprops[0]->total_rows) && $preprops[0]->total_rows > 0) {
+				$destStr .= "'" . $cats->category_name . "',";
+			}
         }
         return substr($destStr, 0, -1);
     }
