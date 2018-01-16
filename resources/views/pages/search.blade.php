e9ac6fa140d4fe6f08f328d3dbed1009f3f77a78
Harman <harman@number7even.de>
2018-01-16 11:54:01 +0530

    detail page

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -560,7 +560,7 @@
             width: 0%;
         }
         .post-filter-inputs:hover, .post-filter-inputs:active, .post-filter-inputs:focus {
-            background-color: #dadad5
+            background-color: #dadad5;
             border-left: 4px solid #a1a39c;
             color: black !important;
             transition: all 0.3s ease 0s;
