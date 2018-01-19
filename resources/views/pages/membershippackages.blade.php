bea41f9469dd65660b0ddcbb1f47811e1b40e873
Sachin <sachin@number7even.de>
2018-01-19 15:39:19 +0530

    Packages page

--- resources/views/pages/membershippackages.blade.php
+++ resources/views/pages/membershippackages.blade.php
@@ -833,7 +833,7 @@
                                                 }
                                             });
     </script>
-    
+     
     <script type="text/javascript">
         $(document).on('ready', function () {
             $(".regular").slick({
