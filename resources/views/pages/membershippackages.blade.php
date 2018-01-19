1a45e396b6d92c5638951dccb2d8c1aeeabf3c12
Harman <harman@number7even.de>
2018-01-19 13:27:16 +0530

    membership package

--- resources/views/pages/membershippackages.blade.php
+++ resources/views/pages/membershippackages.blade.php
@@ -578,6 +578,57 @@
 			
 			
         </script>
+        
         <!-- contact email aside -->
+        
+        <script>
+    $(".editorial-image-slider-previous-btn").click(function ( event ) {
+        event.preventDefault();
+        
+        var index = $(this).parent().parent().find(".image-slider li.active").index();
+		$(this).parent().parent().find(".image-slider li.active").removeClass("active");
+        if (index == 0) {
+			var lindex = $(this).parent().parent().find(".image-slider li:last-child").index() +1;
+            $(this).parent().parent().find(".image-slider li:nth-child("+lindex+")").addClass("active");
+			$(this).parent().parent().find(".images-count").html( lindex + " / " + $(this).parent().parent().find(".image-slider li").length);
+        }
+		else
+		{
+			var rlindex = index - 1;
+			$(this).parent().parent().find(".image-slider li:eq("+rlindex+")").addClass("active");
+			$(this).parent().parent().find(".images-count").html( index + " / " + $(this).parent().parent().find(".image-slider li").length);
+		}
+		
+        
+    });
+    
+    $(".editorial-image-slider-next-btn").click(function ( event ) {
+        event.preventDefault();
+
+        var index = $(this).parent().parent().find(".image-slider li.active").index();
+        if (index == $(this).parent().parent().find(".image-slider li:last-child").index()) {
+            index = -1;
+        }
+        $(this).parent().parent().find(".image-slider li.active").removeClass("active");
+        $(this).parent().parent().find(".image-slider li:nth-child(" + (+index + 2) + ")").addClass("active");
+        
+        $(this).parent().parent().find(".images-count").html( (+index + 2) + " / " + $(this).parent().parent().find(".image-slider li").length);
+        
+    });
+
+    setInterval(function () {
+        var index = $(".auto-slider ul.image-slider > li.active").index();
+        if (index == $(".auto-slider ul.image-slider > li:last-child").index()) {
+            index = -1;
+        }
+
+        $(".auto-slider ul.image-slider > li.active").removeClass("active");
+        $(".auto-slider ul.image-slider > li:nth-child(" + (+index + 2) + ")").addClass("active");
+        $(".auto-slider .images-count").html((+index + 2) + " / " + $(".auto-slider ul.image-slider > li").length);
+
+    }, 40000);
+</script>
+        
+        
     </body>
 </html>
