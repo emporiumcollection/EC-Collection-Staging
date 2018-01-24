4ebf262a987a29a4a9de54d91ebc69276e45c06c
Ravinder Singh <ravinder@number7even.de>
2018-01-24 21:17:08 +0530

    updated

--- app/Http/Controllers/FrontEnd/PersonalizedServiceController.php
+++ app/Http/Controllers/FrontEnd/PersonalizedServiceController.php
@@ -12,7 +12,7 @@ class PersonalizedServiceController extends Controller {
         parent::__construct();
         $this->data['pageTitle'] = '';
         $this->data['data'] = CommonHelper::getInfo();
-        $this->data['pageslider'] = \DB::table('tb_pages_sliders')->select( 'slider_title', 'slider_description', 'slider_img', 'slider_link', 'slider_video', 'slide_type')->where('slider_page_id', 3)->get();
+        $this->data['pageslider'] = \DB::table('tb_pages_sliders')->select( 'slider_title', 'slider_description', 'slider_img', 'slider_link', 'slider_video', 'slide_type')->where('slider_page_id', 35)->get();
     }
     
     /* Method : Index
