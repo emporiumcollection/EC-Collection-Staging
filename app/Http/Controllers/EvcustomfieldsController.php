6f98af6b86bf2d9792dc703fc1040e48a26b61f3
Riaan Kleynhans <riaan@number7even.com>
2018-01-25 07:33:55 +0000

    added new module custom fields

---
+++ app/Http/Controllers/EvcustomfieldsController.php
@@ -0,0 +1,95 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Controllers\controller;
+use App\Models\Evcustomfields;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator as Paginator;
+use Validator, Input, Redirect ; 
+
+
+class EvcustomfieldsController extends Controller {
+
+	protected $layout = "layouts.main";
+	protected $data = array();	
+	public $module = 'evcustomfields';
+	static $per_page	= '10';
+
+	public function __construct()
+	{
+		
+		$this->beforeFilter('csrf', array('on'=>'post'));
+		$this->model = new Evcustomfields();
+		$this->info = $this->model->makeInfo( $this->module);
+		$this->access = $this->model->validAccess($this->info['id']);
+	
+		$this->data = array(
+			'pageTitle'	=> 	$this->info['title'],
+			'pageNote'	=>  $this->info['note'],
+			'pageModule'=> 'evcustomfields',
+			'return'	=> self::returnUrl()
+			
+		);
+		
+	}
+
+	public function getIndex( Request $request )
+	{
+
+		if($this->access['is_view'] ==0) 
+			return Redirect::to('dashboard')
+				->with('messagetext', \Lang::get('core.note_restric'))->with('msgstatus','error');
+
+		
+		return view('evcustomfields.index',$this->data);
+	}	
+
+
+
+	function getUpdate(Request $request, $id = null)
+	{
+	
+		if($id =='')
+		{
+			if($this->access['is_add'] ==0 )
+			return Redirect::to('dashboard')->with('messagetext',\Lang::get('core.note_restric'))->with('msgstatus','error');
+		}	
+		
+		if($id !='')
+		{
+			if($this->access['is_edit'] ==0 )
+			return Redirect::to('dashboard')->with('messagetext',\Lang::get('core.note_restric'))->with('msgstatus','error');
+		}				
+				
+		$this->data['access']		= $this->access;
+		return view('evcustomfields.form',$this->data);
+	}	
+
+	public function getShow( $id = null)
+	{
+	
+		if($this->access['is_detail'] ==0) 
+			return Redirect::to('dashboard')
+				->with('messagetext', \Lang::get('core.note_restric'))->with('msgstatus','error');
+					
+		
+		$this->data['access']		= $this->access;
+		return view('evcustomfields.view',$this->data);	
+	}	
+
+	function postSave( Request $request)
+	{
+		
+	
+	}	
+
+	public function postDelete( Request $request)
+	{
+		
+		if($this->access['is_remove'] ==0) 
+			return Redirect::to('dashboard')
+				->with('messagetext', \Lang::get('core.note_restric'))->with('msgstatus','error');
+		
+	}			
+
+
+}
