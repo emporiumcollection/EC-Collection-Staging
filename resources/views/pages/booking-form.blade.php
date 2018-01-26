5be238b6f4f78363fb01a81526a768cb075295a8
Harman <harman@number7even.de>
2018-01-26 16:55:25 +0530

    search page

--- resources/views/pages/booking-form.blade.php
+++ resources/views/pages/booking-form.blade.php
@@ -50,11 +50,6 @@
             .your-contact-detail-form .display-inline-block > div.text-danger {
                 color: #a94442;
             }
-            
-            .booking-form-select-inputs-style option {
-                background: #252525;
-                color: #fff;
-            }
             .date-picker-wrapper.no-shortcuts.no-gap.two-months {
                 top: 585px !important;
             }
@@ -149,13 +144,6 @@ $(function () {
                                                     <option {{ ($adults!='' && $adults==1) ? 'selected' : '' }}>1</option>
                                                     <option {{ ($adults!='' && $adults==2) ? 'selected' : '' }}>2</option>
                                                     <option {{ ($adults!='' && $adults==3) ? 'selected' : '' }}>3</option>
-                                                    <option {{ ($adults!='' && $adults==4) ? 'selected' : '' }}>4</option>
-                                                    <option {{ ($adults!='' && $adults==5) ? 'selected' : '' }}>5</option>
-                                                    <option {{ ($adults!='' && $adults==6) ? 'selected' : '' }}>6</option>
-                                                    <option {{ ($adults!='' && $adults==7) ? 'selected' : '' }}>7</option>
-                                                    <option {{ ($adults!='' && $adults==8) ? 'selected' : '' }}>8</option>
-                                                    <option {{ ($adults!='' && $adults==9) ? 'selected' : '' }}>9</option>
-                                                    <option {{ ($adults!='' && $adults==10) ? 'selected' : '' }}>10</option>
                                                 </select>
                                             </div>
                                             <div class="right-input-align2">
@@ -165,13 +153,6 @@ $(function () {
                                                     <option {{ ($childs!='' && $childs==1) ? 'selected' : '' }}>1</option>
                                                     <option {{ ($childs!='' && $childs==2) ? 'selected' : '' }}>2</option>
                                                     <option {{ ($childs!='' && $childs==3) ? 'selected' : '' }}>3</option>
-                                                    <option {{ ($childs!='' && $childs==4) ? 'selected' : '' }}>4</option>
-                                                    <option {{ ($childs!='' && $childs==5) ? 'selected' : '' }}>5</option>
-                                                    <option {{ ($childs!='' && $childs==6) ? 'selected' : '' }}>6</option>
-                                                    <option {{ ($childs!='' && $childs==7) ? 'selected' : '' }}>7</option>
-                                                    <option {{ ($childs!='' && $childs==8) ? 'selected' : '' }}>8</option>
-                                                    <option {{ ($childs!='' && $childs==9) ? 'selected' : '' }}>9</option>
-                                                    <option {{ ($childs!='' && $childs==10) ? 'selected' : '' }}>10</option>
                                                 </select>
                                             </div>
                                             <div class="clearfix"></div>
