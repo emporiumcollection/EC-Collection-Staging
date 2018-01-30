a8a39ad26188c324a9df7ea85a71a5b52fde8d56
Manu Mahesh <manu@number7even.de>
2018-01-30 12:26:00 +0530

    CRM Layout Module

--- app/Http/Controllers/CrmlayoutController.php
+++ app/Http/Controllers/CrmlayoutController.php
@@ -105,8 +105,8 @@ class CrmlayoutController extends Controller {
                                 ->select('ai_crm_elements.*')->where('ai_crm_elements.parent_id','=','0')->where('ai_crm_elements.row_id','=',$all_row['crm_row_id'])->where('ai_crm_elements.group_id','=',$all_other_group['crm_group_id'])->orderBy('ai_crm_elements.sort_order','ASC')->get();
                         
                         $custom_fields = Module::join('ai_crm_groups','ai_crm_groups.idmod_mfg','=','tb_module.module_id')
-                                                ->join('ai_crm_customfield','ai_crm_customfield.idmfg_mcf','ai_crm_groups.crm_group_id')
-                                                ->join('ai_crm_rows','ai_crm_rows.crm_row_id','ai_crm_groups.row_id')
+                                                ->join('ai_crm_customfield','ai_crm_customfield.idmfg_mcf','=','ai_crm_groups.crm_group_id')
+                                                ->join('ai_crm_rows','ai_crm_rows.crm_row_id','=','ai_crm_groups.row_id')
                                                 ->select('ai_crm_rows.*', 'ai_crm_groups.*', 'ai_crm_rows.columns AS row_columns', 'ai_crm_groups.title_mfg', 'crm_customfield_id','title_mcf','slug_mcf','idmfg_mcf','option_mcf')->where('ai_crm_groups.crm_group_id','=',$all_other_group['crm_group_id'])->orderBy('orderby_mcf','asc')->get();
                         
                         $all_other_groups[$key]['custom_fields'] = array();
