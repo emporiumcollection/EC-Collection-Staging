d2804681b0ea1209ab909cdaa45743e7dd607c55
Ravinder Singh <ravinder@number7even.de>
2018-01-31 20:02:08 +0530

    show slider

--- app/Http/Controllers/FrontEnd/HotelMembershipController.php
+++ app/Http/Controllers/FrontEnd/HotelMembershipController.php
@@ -12,7 +12,7 @@ class HotelMembershipController extends Controller {
         parent::__construct();
         $this->data['pageTitle'] = '';
         $this->data['data'] = CommonHelper::getInfo();
-        $this->data['pageslider'] = \DB::table('tb_pages_sliders')->select( 'slider_title', 'slider_description', 'slider_img', 'slider_link', 'slider_video', 'slide_type')->where('slider_page_id', 2)->get();
+        $this->data['pageslider'] = \DB::table('tb_pages_sliders')->select( 'slider_title', 'slider_description', 'slider_img', 'slider_link', 'slider_video', 'slide_type')->where('slider_page_id', 107)->get();
 
         print_r($this->data['pageslider']);
     }
