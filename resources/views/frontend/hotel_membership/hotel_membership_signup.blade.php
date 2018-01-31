d49eb90da06f8b8550f661b431552be76b4a26ab
Ravinder Singh <ravinder@number7even.de>
2018-01-31 20:33:33 +0530

    added script

--- resources/views/frontend/hotel_membership/hotel_membership_signup.blade.php
+++ resources/views/frontend/hotel_membership/hotel_membership_signup.blade.php
@@ -15,3 +15,15 @@
 <!-- Custom style -->
 <link href="{{ asset('sximo/assets/memform/css/custom-ai.css')}}" rel="stylesheet" type="text/css"/>
 @endsection
+
+@section('script')
+
+<script type="text/javascript" src="{{ asset('sximo/assets/memform/js/smooth-scroll.js')}}"></script>
+        <!-- animation -->
+        <script type="text/javascript" src="{{ asset('sximo/assets/memform/js/wow.min.js')}}"></script>
+        <!-- swiper carousel -->
+        <script type="text/javascript" src="{{ asset('sximo/assets/memform/js/swiper.min.js')}}"></script>
+
+        <!-- images loaded -->
+        <script type="text/javascript" src="{{ asset('sximo/assets/memform/js/imagesloaded.pkgd.min.js')}}"></script>
+@endsection
