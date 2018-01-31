948aaf33b8137ec00ef6fd170ad9e1e48e95a77c
Ravinder Singh <ravinder@number7even.de>
2018-01-31 20:02:40 +0530

    remove print

--- app/Http/Controllers/FrontEnd/HotelMembershipController.php
+++ app/Http/Controllers/FrontEnd/HotelMembershipController.php
@@ -14,7 +14,6 @@ class HotelMembershipController extends Controller {
         $this->data['data'] = CommonHelper::getInfo();
         $this->data['pageslider'] = \DB::table('tb_pages_sliders')->select( 'slider_title', 'slider_description', 'slider_img', 'slider_link', 'slider_video', 'slide_type')->where('slider_page_id', 107)->get();
 
-        print_r($this->data['pageslider']);
     }
     
     /* Method : Index
