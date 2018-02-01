7153128ac8072b9d113d54219b35b8eaa2500b53
Ravinder Singh <ravinder@number7even.de>
2018-02-01 14:16:00 +0530

    updated

--- resources/views/frontend/hotel_membership/hotel_membership_signup.blade.php
+++ resources/views/frontend/hotel_membership/hotel_membership_signup.blade.php
@@ -11,19 +11,19 @@
                                         <div class="stepwizard">
                                 <div class="stepwizard-row setup-panel">
                                     <div class="stepwizard-step">
-                                        <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
+                                        <a type="button" class="btn btn-primary btn-circle cursor">1</a>
                                         <p>Step 1</p>
                                     </div>
                                     <div class="stepwizard-step">
-                                        <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
+                                        <a type="button" class="btn btn-default btn-circle cursor" disabled="disabled">2</a>
                                         <p>Step 2</p>
                                     </div>
                                     <div class="stepwizard-step">
-                                        <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
+                                        <a  type="button" class="btn btn-default btn-circle cursor" disabled="disabled">3</a>
                                         <p>Step 3</p>
                                     </div>
                                     <div class="stepwizard-step">
-                                        <a href="#step-4" type="button" class="btn btn-default btn-circle" disabled="disabled">4</a>
+                                        <a type="button" class="btn btn-default btn-circle cursor" disabled="disabled">4</a>
                                         <p>Step 4</p>
                                     </div>
                                 </div>
