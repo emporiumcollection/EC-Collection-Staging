031eb78984ba16bb14504b0d6552d601c44afe7a
Harman <harman@number7even.de>
2018-02-02 12:05:58 +0530

    packages page

--- resources/views/frontend/advertiser/advertisement_packages.blade.php
+++ resources/views/frontend/advertiser/advertisement_packages.blade.php
@@ -183,23 +183,17 @@
                 }
 		.image-slider-container .editorial-image-slider-btns {
 			margin-top: -100px !important;
+                        width: 94%;
 		}
-
-                /* my */
-/*                .image-slider-container .editorial-image-slider-btns {
-                        margin-top: -195px !important;
-                }*/
-                
-                /* my */
-/*                .image-slider-container .editorial-images-count {
-                    color: #fff;
-                    float: left;
-                    font-size: 33px;
-                    margin-left: 42%;
-                    margin-top: -256px;
-                    position: relative;
-                    width: 22%;
-                }*/
+                .image-slider-container .editorial-images-count {
+                        color: #fff;
+                        float: left;
+                        font-size: 33px;
+                        margin-left: 39%;
+                        margin-top: -155px;
+                        position: relative;
+                        width: 22%;
+                }
 		header.haside {
 			right: 0 !important;
 			position: fixed;
