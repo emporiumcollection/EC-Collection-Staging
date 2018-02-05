912e361d8f428220ef87db6c4f0a41b0ac8b7733
Harman <harman@number7even.de>
2018-02-05 12:29:27 +0530

    servise page styling

--- resources/views/frontend/personalized/personalized_service.blade.php
+++ resources/views/frontend/personalized/personalized_service.blade.php
@@ -3,7 +3,7 @@
 @section('css')
 <link href="{{ asset('sximo/assets/css/chosen.css')}}" rel="stylesheet" type="text/css"/>
 <link href="{{ asset('sximo/assets/css/personalized.css')}}" rel="stylesheet" type="text/css"/>
-<link href="{{ asset('sximo/assets/memform/css/custom-ai.css')}}" rel="stylesheet" type="text/css"/>
+<!--<link href="{{ asset('sximo/assets/memform/css/custom-ai.css')}}" rel="stylesheet" type="text/css"/>-->
 @stop
 @section('content')
         <div class="wrapper">
@@ -24,32 +24,48 @@
                         </ul>
                     </div>-->
                     <!-- steps start -->
-                    <div class="stepwizard">
-                        <div class="stepwizard-row setup-panel">
-                            <div class="stepwizard-step">
-                                <a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
-                                <p>Step 1</p>
-                            </div>
-                            <div class="stepwizard-step">
-                                <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
-                                <p>Step 2</p>
-                            </div>
-                            <div class="stepwizard-step">
-                                <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
-                                <p>Step 3</p>
-                            </div>
-                            <div class="stepwizard-step">
-                                <a href="#step-4" type="button" class="btn btn-default btn-circle" disabled="disabled">4</a>
-                                <p>Step 4</p>
-                            </div>
-                        </div>
-                    </div>
+                   <div class="wizard-inner">
+                <div class="connecting-line"></div>
+                <ul class="nav nav-tabs" role="tablist">
+
+                    <li role="presentation" class="active">
+                        <a href="#step1" data-toggle="tab" aria-controls="step1" role="tab" title="Step 1">
+                            <span class="round-tab">
+                                <i class="glyphicon glyphicon-folder-open"></i>
+                            </span>
+                        </a>
+                    </li>
+
+                    <li role="presentation" class="disabled">
+                        <a href="#step2" data-toggle="tab" aria-controls="step2" role="tab" title="Step 2">
+                            <span class="round-tab">
+                                <i class="glyphicon glyphicon-pencil"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li role="presentation" class="disabled">
+                        <a href="#step3" data-toggle="tab" aria-controls="step3" role="tab" title="Step 3">
+                            <span class="round-tab">
+                                <i class="glyphicon glyphicon-picture"></i>
+                            </span>
+                        </a>
+                    </li>
+
+                    <li role="presentation" class="disabled">
+                        <a href="#complete" data-toggle="tab" aria-controls="complete" role="tab" title="Complete">
+                            <span class="round-tab">
+                                <i class="glyphicon glyphicon-ok"></i>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
                     <!-- steps end -->
              
                     <div class="col-md-12 col-sm-8">
                             <form action="{{URL::to('personalized-service/save')}}" method="POST">
-                                <fieldset class="muti-form-align" id="step-1">
-                                    <div class="centred-tab-align">
+                                <fieldset class="muti-form-align">
+                                    <div class="centred-tab-align tab-pane active" role="tabpanel" id="step1">
                                         <div>
                                             <h2 class="black-heading-big">Where do you want to travel?</h2>
                                             <p class="sub-des-heading">You can specify one or more destinations</p>
@@ -94,10 +110,10 @@
                                         </div>
                                     </div>
                                     <div class="clearfix"></div>
-                                    <input type="button" name="next" class="next action-button personalized-btn-deafult progress-bar-btn-increment" value="Continue" />
+                                    <input type="button" name="next" class="next action-button personalized-btn-deafult progress-bar-btn-increment next-step" value="Continue" />
                                 </fieldset>
-                                <fieldset class="hide-form muti-form-align" id="step-2">
-                                    <div class="experience-page-align">
+                                <fieldset class="hide-form muti-form-align">
+                                    <div class="experience-page-align tab-pane" role="tabpanel" id="step2">
                                         <h2 class="black-heading-big">Inspirations</h2>
                                         <div class="selector-outer-align">
                                             <?php
@@ -122,12 +138,12 @@
                                             <div class="clearfix"></div>
                                         </div>
                                     </div>
-                                    <input type="button" name="next" class="next action-button personalized-btn-deafult" value="Continue" />
+                                    <input type="button" name="next" class="next action-button personalized-btn-deafult next-step" value="Continue" />
                                     <div></div>
-                                    <input type="button" name="previous" class="previous action-button  ps-basic-btn progress-bar-btn-decrement" value="Previous" />
+                                    <input type="button" name="previous" class="previous action-button  ps-basic-btn progress-bar-btn-decrement prev-step" value="Previous" />
                                 </fieldset>
                                 <fieldset class="hide-form muti-form-align">
-                                    <div class="experience-page-align">
+                                    <div class="experience-page-align tab-pane" role="tabpanel" id="step2">
                                         <h2 class="black-heading-big">What would you like experience</h2>
                                         <div class="selector-outer-align">
                                             <?php
@@ -155,8 +171,8 @@
                                     <div></div>
                                     <input type="button" name="previous" class="previous action-button  ps-basic-btn" value="Previous" />
                                 </fieldset>
-                                <fieldset class="hide-form muti-form-align" id="step-3">
-                                    <div class="centred-tab-align">
+                                <fieldset class="hide-form muti-form-align">
+                                    <div class="centred-tab-align tab-pane" role="tabpanel" id="step3">
                                         <div>
                                             <h2 class="black-heading-big">What is particularly important to you?</h2>
                                             <p class="sub-des-heading">Tell us what you value - the more detailed the better.</p>
@@ -168,12 +184,12 @@
                                             <a class="custom-tooltip" href="javascript:void(0)" data-toggle="tooltip" data-placement="top" title="Your callback date can be selected in last step"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
                                         </div>
                                     </div>
-                                    <input type="button" name="next" class="next action-button personalized-btn-deafult" value="Continue" />
+                                    <input type="button" name="next" class="next action-button personalized-btn-deafult next-step" value="Continue" />
                                     <div></div>
-                                    <input type="button" name="previous" class="previous action-button  ps-basic-btn progress-bar-btn-decrement" value="Previous" />
+                                    <input type="button" name="previous" class="previous action-button  ps-basic-btn progress-bar-btn-decrement prev-step" value="Previous" />
                                 </fieldset>
-                                <fieldset class="hide-form muti-form-align" id="step-4">
-                                    <div class="centred-tab-align">
+                                <fieldset class="hide-form muti-form-align">
+                                    <div class="centred-tab-align tab-pane" role="tabpanel" id="step4">
                                         <div>
                                             <h2 class="black-heading-big">How many people travel?</h2>
                                         </div>
@@ -232,9 +248,9 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <input type="button" name="next" class="next action-button personalized-btn-deafult" value="Continue" />
+                                    <input type="button" name="next" class="next action-button personalized-btn-deafult next-step" value="Continue" />
                                     <div></div>
-                                    <input type="button" name="previous" class="previous action-button  ps-basic-btn" value="Previous" />
+                                    <input type="button" name="previous" class="previous action-button  ps-basic-btn prev-step" value="Previous" />
                                 </fieldset>
                                 <fieldset class="hide-form muti-form-align">
                                     <div class="centred-tab-align">
@@ -300,6 +316,143 @@
         @stop
         @section('script')
 
+        <style>
+             .wizard .nav-tabs {
+        position: relative;
+        margin: 40px auto;
+        margin-bottom: 0;
+        border-bottom-color: #e0e0e0;
+    }
+
+    .wizard > div.wizard-inner {
+        position: relative;
+    }
+
+.connecting-line {
+    height: 2px;
+    background: #e0e0e0;
+    position: absolute;
+    width: 80%;
+    margin: 0 auto;
+    left: 0;
+    right: 0;
+    top: 50%;
+    z-index: 1;
+}
+
+.wizard .nav-tabs > li.active > a, .wizard .nav-tabs > li.active > a:hover, .wizard .nav-tabs > li.active > a:focus {
+    color: #555555;
+    cursor: default;
+    border: 0;
+    border-bottom-color: transparent;
+}
+
+span.round-tab {
+    width: 70px;
+    height: 70px;
+    line-height: 70px;
+    display: inline-block;
+    border-radius: 100px;
+    background: #fff;
+    border: 2px solid #e0e0e0;
+    z-index: 2;
+    position: absolute;
+    left: 0;
+    text-align: center;
+    font-size: 25px;
+}
+span.round-tab i{
+    color:#555555;
+}
+.wizard li.active span.round-tab {
+    background: #fff;
+    border: 2px solid #5bc0de;
+    
+}
+.wizard li.active span.round-tab i{
+    color: #5bc0de;
+}
+
+span.round-tab:hover {
+    color: #333;
+    border: 2px solid #333;
+}
+
+.wizard .nav-tabs > li {
+    width: 25%;
+}
+
+.wizard li:after {
+    content: " ";
+    position: absolute;
+    left: 46%;
+    opacity: 0;
+    margin: 0 auto;
+    bottom: 0px;
+    border: 5px solid transparent;
+    border-bottom-color: #5bc0de;
+    transition: 0.1s ease-in-out;
+}
+
+.wizard li.active:after {
+    content: " ";
+    position: absolute;
+    left: 46%;
+    opacity: 1;
+    margin: 0 auto;
+    bottom: 0px;
+    border: 10px solid transparent;
+    border-bottom-color: #5bc0de;
+}
+
+.wizard .nav-tabs > li a {
+    width: 70px;
+    height: 70px;
+    margin: 20px auto;
+    border-radius: 100%;
+    padding: 0;
+}
+
+    .wizard .nav-tabs > li a:hover {
+        background: transparent;
+    }
+
+.wizard .tab-pane {
+    position: relative;
+    padding-top: 50px;
+}
+
+.wizard h3 {
+    margin-top: 0;
+}
+
+@media( max-width : 585px ) {
+
+    .wizard {
+        width: 90%;
+        height: auto !important;
+    }
+
+    span.round-tab {
+        font-size: 16px;
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+    }
+
+    .wizard .nav-tabs > li a {
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+    }
+
+    .wizard li.active:after {
+        content: " ";
+        position: absolute;
+        left: 35%;
+    }
+}
+        </style>
 
         <script src=" {{ asset('sximo/assets/js/chosen.jquery.js') }} " type="text/javascript"></script>
         <script src=" {{ asset('sximo/assets/js/init.js') }} " type="text/javascript"></script>
@@ -432,47 +585,40 @@
         
         <script>
             $(document).ready(function () {
-  var navListItems = $('div.setup-panel div a'),
-          allWells = $('.setup-content'),
-          allNextBtn = $('.nextBtn');
+    //Initialize tooltips
+//    $('.nav-tabs > li a[title]').tooltip();
+    
+    //Wizard
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
-  allWells.hide();
+        var $target = $(e.target);
+    
+        if ($target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
 
-  navListItems.click(function (e) {
-      e.preventDefault();
-      var $target = $($(this).attr('href')),
-              $item = $(this);
+    $(".next-step").click(function (e) {
 
-      if (!$item.hasClass('disabled')) {
-          navListItems.removeClass('btn-primary').addClass('btn-default');
-          $item.addClass('btn-primary');
-          allWells.hide();
-          $target.show();
-          $target.find('input:eq(0)').focus();
-      }
-  });
+        var $active = $('.wizard .nav-tabs li.active');
+        $active.next().removeClass('disabled');
+        nextTab($active);
 
-  allNextBtn.click(function(){
-      var curStep = $(this).closest(".setup-content"),
-          curStepBtn = curStep.attr("id"),
-          nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-          curInputs = curStep.find("input[type='text'],input[type='url']"),
-          isValid = true;
+    });
+    $(".prev-step").click(function (e) {
 
-      $(".form-group").removeClass("has-error");
-      for(var i=0; i<curInputs.length; i++){
-          if (!curInputs[i].validity.valid){
-              isValid = false;
-              $(curInputs[i]).closest(".form-group").addClass("has-error");
-          }
-      }
+        var $active = $('.wizard .nav-tabs li.active');
+        prevTab($active);
 
-      if (isValid)
-          nextStepWizard.removeAttr('disabled').trigger('click');
-  });
-
-  $('div.setup-panel div a.btn-primary').trigger('click');
+    });
 });
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}
             
         </script>
 
