7005528c395066b519b198e9a5d5f2d7825583cf
puneetsachdeva-number7even <puneet@number7even.de>
2018-02-05 18:32:25 +0530

    order mod changes

--- app/Http/Controllers/UserorderController.php
+++ app/Http/Controllers/UserorderController.php
@@ -133,6 +133,12 @@ class UserorderController extends Controller {
 		
 		$this->data['id'] = $id;
 		$this->data['access']		= $this->access;
+		
+		$orderdetail = $this->data['row'];
+		$this->data['order_item_detail'] = \DB::table('tb_order_items')->where('order_id', $id)->get();
+		
+		$this->data['userDetail'] = \DB::table('tb_user_company_details')->where('user_id', $orderdetail->user_id)->first();
+		
 		return view('userorder.view',$this->data);	
 	}	
 
@@ -194,5 +200,62 @@ class UserorderController extends Controller {
 
 	}			
 
-
+	public function ordersdownloadinvoicepdf(Request $request, $ordid)
+	{
+		$downFileName = 'order-invoice-'.date('d-m-Y').'.pdf';
+		//$cid = $request->input('contentId');
+		if($ordid!="" && $ordid>0)
+		{
+			$shopitems = array();
+			$fetch_orderItems = \DB::table('tb_shop_order_products')->join('tb_shop_products', 'tb_shop_products.id', '=', 'tb_shop_order_products.product_id')->join('tb_shop_product_categories', 'tb_shop_product_categories.id','=','tb_shop_order_products.shop_cat_id')->select('tb_shop_order_products.*', 'tb_shop_products.title', 'tb_shop_products.description', 'tb_shop_products.price', 'tb_shop_products.custom_description', 'tb_shop_products.custom_description_eng', 'tb_shop_products.description_eng', 'tb_shop_products.title_eng', 'tb_shop_product_categories.cat_name')->where('tb_shop_order_products.shop_order_id', $ordid)->get();
+			if(!empty($fetch_orderItems))
+			{
+				$shopitems = $fetch_orderItems;
+				$currency = \DB::table('tb_settings')->where('key_value', 'default_currency')->first();
+				$html = '<style> .main { margin:0 25px; width:700px; font-family: arial, sans-serif; } .page-break { page-break-after: always; } .header,.footer {width: 100%; position:fixed;} .header { top: 20px; text-align:center;} .footer {bottom: 30px; font-size:10px;} .pagenum:after {content: counter(page);} .imgBox { text-align:center; width:400px; margin:50px auto 30px auto;} .nro { text-align:center; font-size:12px; } .header img { width:250px; height: 50px; } .Mrgtop80 {margin-top:80px;} .Mrgtop40 {margin-top:40px;} .Mrgtop20 {margin-top:10px;} .monimg img { width:125px; height:80px; }  .font13 { font-size:13px; } .font12 { font-size:12px; } .algRgt { text-align:right; } .algCnt { text-align:center; }</style>';
+				$i=1;
+				$html .= '<div class="main"><div class="header"><img src="'. \URL::to('sximo/images/logo_janua_pdf.png').'"></div><br><br><br><div class="footer">© Copyright: Christian Seisenberger Gmbh</div>';
+				
+				$userInfo = \DB::table('tb_users')->where('id', $fetch_orderItems[0]->user_id)->first();
+				$companydet = \DB::table('tb_user_company_details')->where('user_id', $fetch_orderItems[0]->user_id )->first();
+				$html .= '<div class="Mrgtop40 font13"><table><tr><td width="250"> JANUA-Daten : </td> <td width="20"></td> <td width="250"> User-Daten : </td> </tr> <tr><td valign="top"> Christian Seisenberger GmbH <br><br> Am Klosterpark 1 <br> 84427, Armstorf <br> Deutschland <br><br> Telefon: +49 (0)80 81 - 95 46 80 <br> Telefax: +49 (0)80 81 - 95 43 31 <br> E-Mail: info@janua-moebel.com </td> <td></td>';
+				if(!empty($companydet))
+				{
+					$html .= '<td> '.$companydet->company_name.'<br><br>'.$companydet->company_address .' . '.$companydet->company_address2 .' <br> '. $companydet->company_postal_code .', '.$companydet->company_city .' <br> '.$companydet->company_country.'<br><br>Telefon: '.$companydet->company_phone.'<br>E-Mail: '.$companydet->company_email.'</td>';
+				}
+				else{
+					$html .= '<td></td>';
+				}
+				$html .='</tr> </table></div>';
+				$html .= '<div class="Mrgtop80 font13"><table><tr style="background:#eeeeee;"><th width="100" class="alnCenter">Kategorie</th><th width="280" class="alnCenter">Produkt </th><th width="50" class="alnCenter">Anzahl </th><th width="80" class="alnCenter">Preis </th></tr>';
+				$qtyPr = 1;
+				$Totprice = 0;
+				foreach($fetch_orderItems as $orderItems)
+				{
+					if(\Session::get('newlang')=='English')
+					{
+						$html .= '<tr><td>'.$orderItems->cat_name.'</td><td><b>'.$orderItems->title_eng.'</b><br>'.$orderItems->description_eng.'</td><td class="alnCenter">'.$orderItems->product_qty.'</td><td class="alnCenter">'.$orderItems->price.'</td></tr>';
+					}
+					else{
+						$html .= '<tr><td>'.$orderItems->cat_name.'</td><td><b>'.$orderItems->title.'</b><br>'.$orderItems->description.'</td><td class="algCnt">'.$orderItems->product_qty.'</td><td class="algCnt">'.$currency->content .' '.number_format($orderItems->price, 2, '.', ',').'</td></tr>';
+					}
+					
+					$qtyPr = $orderItems->price * $orderItems->product_qty;
+					$Totprice = $Totprice + $qtyPr;
+				}
+				$html .= '<tr><td colspan="3" style="text-align:right;"><b>Gesammtsumme<b></td><td class="algCnt font13"><b>'.$currency->content .' '.number_format($Totprice, 2, '.', ',').'<b></td></tr>';
+				$html .= '</table></div>';
+				
+				$pdf = \App::make('dompdf.wrapper');
+				$pdf->loadHTML($html);
+				return $pdf->download($downFileName);
+			}
+			else{
+				return 'error';
+			}
+		}
+		else{
+			return 'error';
+		}
+	}
 }
--- resources/views/userorder/view.blade.php
+++ resources/views/userorder/view.blade.php
@@ -19,10 +19,7 @@
 
 <div class="sbox animated fadeInRight">
 	<div class="sbox-title"> 
-   		<a href="{{ URL::to('userorder?return='.$return) }}" class="tips btn btn-xs btn-default pull-right" title="{{ Lang::get('core.btn_back') }}"><i class="fa fa-arrow-circle-left"></i>&nbsp;{{ Lang::get('core.btn_back') }}</a>
-		@if($access['is_add'] ==1)
-   		<a href="{{ URL::to('userorder/update/'.$id.'?return='.$return) }}" class="tips btn btn-xs btn-primary pull-right" title="{{ Lang::get('core.btn_edit') }}"><i class="fa fa-edit"></i>&nbsp;{{ Lang::get('core.btn_edit') }}</a>
-		@endif 
+   		<a href="{{ URL::to('userorder?return='.$return) }}" class="tips btn btn-xs btn-default pull-right" title="{{ Lang::get('core.btn_back') }}"><i class="fa fa-arrow-circle-left"></i>&nbsp;{{ Lang::get('core.btn_back') }}</a> 
 	</div>
 	<div class="sbox-content" style="background:#fff;"> 	
 
@@ -30,45 +27,61 @@
 			<tbody>	
 		
 					<tr>
-						<td width='30%' class='label-view text-right'>Id</td>
-						<td>{{ $row->id }} </td>
+						<td width='80%' class='label-view text-right'>Order Id</td>
+						<td># {{ $row->id }} </td>
 						
 					</tr>
-				
+					
 					<tr>
-						<td width='30%' class='label-view text-right'>Status</td>
-						<td>{{ $row->status }} </td>
-						
-					</tr>
-				
-					<tr>
-						<td width='30%' class='label-view text-right'>Comments</td>
-						<td>{{ $row->comments }} </td>
-						
-					</tr>
-				
-					<tr>
-						<td width='30%' class='label-view text-right'>User Id</td>
-						<td>{{ $row->user_id }} </td>
-						
-					</tr>
-				
-					<tr>
-						<td width='30%' class='label-view text-right'>Created</td>
+						<td width='80%' class='label-view text-right'>Created</td>
 						<td>{{ $row->created }} </td>
 						
 					</tr>
 				
 					<tr>
-						<td width='30%' class='label-view text-right'>Updated</td>
+						<td width='80%' class='label-view text-right'>Updated</td>
 						<td>{{ $row->updated }} </td>
 						
 					</tr>
+					
+					<tr>
+						<td width='80%' class='label-view text-right'>User Name</td>
+						<td>{!! SiteHelpers::gridDisplayView($row->user_id,'user_id','1:tb_users:id:first_name|last_name') !!} </td>
+						
+					</tr>
+					
+					@if(!empty($userDetail))
+						<tr>
+							<td width='80%' class='label-view text-right'>User Address</td>
+							<td>{{ $userDetail->company_address.' '.$userDetail->company_address2.' '.$userDetail->company_city.' '.$userDetail->company_postal_code.' '.$userDetail->company_country  }}</td>
+							
+						</tr>
+					@endif
 				
+					<tr>
+						<td width='80%' class='label-view text-right'>Status</td>
+						<td>{{ $row->status }} </td>
+						
+					</tr>
+				
+					<tr>
+						<td width='80%' class='label-view text-right'>Comments</td>
+						<td>{{ $row->comments }} </td>
+						
+					</tr>
 			</tbody>	
 		</table>   
 
-	 
+		@if(!empty($order_item_detail))
+			<div id="item-pnl">
+				<div class="row items-pnl-head">
+					<div class="col-sm-3 col">PACKAGES</div>
+					<div class="col-sm-1 col">QTY</div>
+					<div class="col-sm-1 col">PRICE</div>
+					<div class="col-sm-7 col">DATA</div>
+				</div>
+			</div>
+		@endif
 	
 	</div>
 </div>	
