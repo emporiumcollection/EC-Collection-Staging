828d69dbc156d96e652c806c612c9ad292ad3779
Manu Mahesh <manu@number7even.de>
2018-02-08 15:19:45 +0530

    CRM Layout Module

--- resources/views/layouts/crm_layout/ai_crm_vc.blade.php
+++ resources/views/layouts/crm_layout/ai_crm_vc.blade.php
@@ -800,7 +800,7 @@ $fieldArray['file'] = 'File';
                     <div class="form-group">
                         <div class="form-group form-md-line-input form-md-floating-label has-info">
                             {!! Form::label('table_mob', 'Field Type')  !!}
-                            <select class="custom-field-types form-control" name="type" >
+                            <select class="custom-field-types form-control select2" name="type" >
                                 @foreach($fieldArray as $fieldKey=>$fieldMatch)
                                 <option value="{{$fieldKey}}" >{{$fieldMatch}}</option>
                                 @endforeach
