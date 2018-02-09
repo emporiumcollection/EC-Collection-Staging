0398477d481c36fa2db6a2c416175ee2bcc3f9fb
ram <ram@number7even.de>
2018-02-09 19:40:58 +0530

    Final

--- app/Http/Controllers/UserorderController.php
+++ app/Http/Controllers/UserorderController.php
@@ -388,10 +388,10 @@ class UserorderController extends Controller {
 				<table width="100%">
 				 <tr>
 					<td colspan="2" align="right"  height="35px;">
-						
-					<hr style="border-top:1px solid #000; width:100%; "/></td>
+						&nbsp;
+					</td>
 				 </tr>
-					<tr >
+					<tr style="border-top:1px solid #000;">
 						<td width="50%">';
 							$html .= 'Tel: '.$invoice_phone_num->content . ' email: ' .$invoice_email_id->content;
 				$html .= '</td>
