2b9e80c37acf23a5e91a0b9e9d75d77a4cafed28
ram <ram@number7even.de>
2018-02-09 13:42:47 +0530

    bg color

--- app/Http/Controllers/UserorderController.php
+++ app/Http/Controllers/UserorderController.php
@@ -311,15 +311,13 @@ class UserorderController extends Controller {
 						.valin{ vertical-align:top;} 
 						.valinbt{ vertical-align:bottom; text-align:right;}
 
-						body {
-						  background: rgb(204,204,204); 
-						}
+						
 						.page {
 						  background: white;
 						  display: block;
 						  margin: 0 auto;
 						  margin-bottom: 0.5cm;
-						  box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
+						  
 						}
 						.page[size="A4"] {  
 						  width: 21cm;
