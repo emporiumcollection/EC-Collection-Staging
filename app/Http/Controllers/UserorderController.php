58b6b6941c06bb9713bc95e15bb123ebf6e58544
ram <ram@number7even.de>
2018-02-09 19:44:25 +0530

    test

--- app/Http/Controllers/UserorderController.php
+++ app/Http/Controllers/UserorderController.php
@@ -283,6 +283,7 @@ class UserorderController extends Controller {
 				$html = '<style> 
 						.main { margin:2px; width:100%; font-family: arial, sans-serif; } 
 						.page-break { page-break-after: always; } 
+						.topDIV{top: 310px; text-align:center;}
 						.header{ width: 100%; position:fixed; top: -35px; text-align:center; height:200px;} 
 						.footer {width: 100%; position:fixed;} 
 						.pagenum:after {content: counter(page);} 
@@ -384,7 +385,7 @@ class UserorderController extends Controller {
 				$html .= '</td></tr></table></div>';
 				
 				$html .= '
-				<div>
+				<div class="topDIV">
 				<table width="100%" border="1px">
 				 <tr>
 					<td colspan="2" align="right"  height="35px;">
