0f3ddb5eb9c4ca618d9997c55203753f878efe8f
puneetsachdeva-number7even <puneet@number7even.de>
2018-02-21 14:53:05 +0530

    template update

--- resources/views/user/emails/crm_email.blade.php
+++ resources/views/user/emails/crm_email.blade.php
@@ -1,12 +1,12 @@
 Hi
 
 {!! $msg !!}
-
+<br><br>
 Please click on download button to download the file
-
+<br>
 <a href="{{ URL::to($link) }}">Download</a>
-
-if button does not work please copy below link and paste into browser.
+<br>
+if button does not work please copy below link and paste into browser.<br>
 {{ URL::to($link) }}
-
+<br><br>
 Thanks
