b0e473831cbb06a5fc03d6167775acc501214570
Manu Mahesh <manu@number7even.de>
2018-02-22 14:41:31 +0530

    Quality Assurance Module

--- app/Models/Qualityassurance.php
+++ app/Models/Qualityassurance.php
@@ -15,7 +15,7 @@ class qualityassurance extends Sximo  {
 
 	public static function querySelect(  ){
 		
-		return "  SELECT tb_quality_assurance.*, tb_properties.property_name AS hotel_name, tb_properties.city AS hotel_city, CONCAT((tb_users.first_name), (' '), (tb_users.last_name)) AS hotel_manager_name, , CONCAT((user2.first_name), (' '), (user2.last_name)) AS quality_assurer_name FROM tb_quality_assurance LEFT JOIN tb_properties ON tb_properties.id = tb_quality_assurance.property_id LEFT JOIN tb_users ON tb_users.id = tb_quality_assurance.hotel_manager LEFT JOIN tb_users AS user2 ON user2.id = tb_quality_assurance.quality_assurer  ";
+		return "  SELECT tb_quality_assurance.*, tb_properties.property_name AS hotel_name, tb_properties.city AS hotel_city, CONCAT((tb_users.first_name), (' '), (tb_users.last_name)) AS hotel_manager_name, CONCAT((user2.first_name), (' '), (user2.last_name)) AS quality_assurer_name FROM tb_quality_assurance LEFT JOIN tb_properties ON tb_properties.id = tb_quality_assurance.property_id LEFT JOIN tb_users ON tb_users.id = tb_quality_assurance.hotel_manager LEFT JOIN tb_users AS user2 ON user2.id = tb_quality_assurance.quality_assurer  ";
 	}	
 
 	public static function queryWhere(  ){
