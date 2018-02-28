66ac93ac92104e24de0bf6b87d0b8c2f54448f69
Manu Mahesh <manu@number7even.de>
2018-02-28 17:58:06 +0530

    Quality Assurance Module

--- resources/views/qualityassurance/defaults.blade.php
+++ resources/views/qualityassurance/defaults.blade.php
@@ -1,6 +1,7 @@
 <?php
 global $_bedrooms;
 $_bedrooms = array();
+
 $_bedrooms[] = array('standard' => 'Was the décor luxurious/current in appearance and well coordinated throughout the room?',
                     'performance_classificatoon' => 'PRODUCT',
                     'meet' => '',
@@ -15,16 +16,301 @@ $_bedrooms[] = array('standard' => 'Did the room provide an abundance of space (
                     'na' => '',
                     'note' => '',
                 );
-
-global $_bathrooms;
-$_bathrooms = array();
-$_bathrooms[] = array('standard' => 'Was the décor luxurious/current in appearance and well coordinated throughout the room?',
+$_bedrooms[] = array('standard' => 'Was the flooring of high quality (i.e. high grade wood, marble, stone, granite, elegant area rugs or high density carpet) and was it clean and in good repair?',
                     'performance_classificatoon' => 'PRODUCT',
                     'meet' => '',
                     'below' => '',
                     'na' => '',
                     'note' => '',
                 );
+$_bedrooms[] = array('standard' => 'Were the wall surfaces well maintained and enhanced with several design elements (i.e. wood baseboards, ceiling trim, soft wall coverings, wood panelling, etc.)',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were all areas of the room extremely well lit (i.e. both sides of the bed, at desk and sitting area) with custom high quality light fixtures?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was individually controlled lighting available from the bed and could all room lights be switched off from the bed?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was LED lighting within the room minimized in a way that it did not interfere with guest\'s sleep? ',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was all furniture well maintained and of a high quality (i.e. solid wood, upscale fabrics, etc.)?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were all fabrics (curtains, upholstery, bedspread) fresh in appearance and of good quality?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were two chairs available (in the case of double occupancy) that were of the appropriate height for a room service trolley?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were an occasional table and seating provided in the room?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was the desk/workspace of a good size and conducive to business needs (i.e. multiple, easily accessible unused electrical outlets)?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was the room adequately soundproofed?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was an adjustable climate control panel provided which was appropriate to the destination and if so, was it quiet and in good working order?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Did the curtains offer a complete blackout when closed?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was the pillows and mattress comfortable and the bed linen of good quality (i.e. 100% cotton)?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was there ample well lit full hanging (i.e. for dresses) and drawer/shelf space available for a minimum of two guests?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were matching good quality hangers provided (i.e. wooden and not anti-theft) and did they consist of suit, padded and clip hangers?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was a flat screen television present and was the screen a minimum of 42 inches?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was television/remote control in good condition/working order and did it offer multilingual channels as well as high definition channels and either a printed or television/iPad enabled channel guide?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was there a movie on demand channel option, a DVD/CD player provided or was it possible to stream media content to the television? ',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was all technology (e.g. iPad, sound system, electric curtains, etc.) and lighting in good working order and easy to use? ',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was high-speed Wi-Fi available and if so was it easy to access and did it provide a seamless wireless experience (i.e. strong and continuous connection without requirement to log in more than once)? ',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was the facility provided to connect a MP3/iPhone/iPod/Android to a speaker within the room?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Did Wi-Fi facility offer the ability to connect multiple devices without incurring additional charges (e.g. minimum of three devices)?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was the door lock in good condition with a deadbolt or safety chain?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was an upscale comprehensive guest services directory provided (e.g. printed copy, iPad or via television), and was it up to date, grammatically correct and in good condition?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was a range of good quality stationery provided (minimum of envelopes and hotel paper)?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were a shoehorn (urban hotels only) and umbrella available (umbrella requirement is destination specific and can be available in the room or offered at the hotel entrance if weather is inclement)?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was a fabric laundry bag and list provided?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was a full-length mirror present within the room?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were there spare electrical points near a mirror (e.g. for hairdryer) and were at least two spare power points available at the bedside table (e.g. for mobile phones)?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were either a range of international electrical sockets provided at the desk or were international adapters present in the room or alternatively was an adaptor offered on arrival to international guests? ',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was a well-stocked in-room bar present and was an option available to customise the contents to the guest\'s personal preferences?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was an in-room safe provided and was it of an adequate size (i.e. large enough for a laptop) and in good working order/available for immediate use (i.e. not locked)?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were there either three telephones in the room (i.e. desk, beside and bathroom) or alternatively two telephones, one of which must be cordless?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were all room telephones in good working order with speed dial, voicemail, message waiting and conference speaker facilities?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was a selection of reading material provided (i.e. current lifestyle magazines and/or books)?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Was a fresh floral/plant decoration present in the room or bathroom?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'If provided, was the welcome note personalized?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bedrooms[] = array('standard' => 'Were a coffee machine, kettle and amenities (i.e. sugar, milk, cups etc.) provided or available via in-room dining/butler service on a complimentary basis?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+
+/**************************************************************************/
+
+global $_bathrooms;
+$_bathrooms = array();
+
+$_bathrooms[] = array('standard' => 'Was the décor luxurious in appearance and well coordinated throughout the bathroom?',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+$_bathrooms[] = array('standard' => 'Did the bathroom provide an abundance of space in a way that allowed freedom of movement for two guests? ',
+                    'performance_classificatoon' => 'PRODUCT',
+                    'meet' => '',
+                    'below' => '',
+                    'na' => '',
+                    'note' => '',
+                );
+
+/*********************************************************************************/
 
 global $_exterior_grounds;
 $_exterior_grounds = array();
