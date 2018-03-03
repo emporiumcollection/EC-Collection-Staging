0bc4d9b2bb530d65ba5d8e32707431645237fcd7
Ravinder Singh <ravinder@number7even.de>
2018-03-03 15:04:59 +0530

    test

--- app/Http/Controllers/ImportdataController.php
+++ app/Http/Controllers/ImportdataController.php
@@ -8,13 +8,22 @@ use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 use Validator, Input, Redirect,File;
 
-class ImportdataController extends Controller {
+class ImportdataController extends Controller { 
 
 
 
 	public function getResto(Request $request) {
-		$data = Properties::paginate(1);
-		dd($data);
+
+		$data = Properties::paginate(5);
+		$count = 1; 
+		foreach($data as $val){
+			echo $val->property_name;
+			echo '<br>';
+			$count++;
+		}
+		if($count>5){
+			header("refresh: 3;");
+		}
 	}
 
 	public function getSpa(Request $request) {
