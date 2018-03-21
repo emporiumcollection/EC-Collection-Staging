a0f97432ecf6dd630d816c072c57d07417ebd5e5
Ravinder Singh <I>
2018-03-21 17:49:40 +0530

    added new sections

--- resources/views/frontend/themes/emporium/layouts/home.blade.php
+++ resources/views/frontend/themes/emporium/layouts/home.blade.php
@@ -4,6 +4,9 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="@yield('meta_keywords') ">
+        <meta name="description" content="@yield('meta_description') ">
+
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<title> @yield('title') | {{ CNF_APPNAME }}</title>
 		<!-- Bootstrap -->
