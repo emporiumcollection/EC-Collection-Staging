76240dec5987d126422d4460a2f5b8c3dc2ea0e0
Ravinder Singh <ravinder@dheerajtech.com>
2018-03-22 12:03:58 +0530

    added new section

--- resources/views/frontend/themes/emporium/pages/index.blade.php
+++ resources/views/frontend/themes/emporium/pages/index.blade.php
@@ -54,6 +54,12 @@
       
 @endsection
 
+{{--For Right Side Icons --}}
+      @section('right_side_iconbar')
+      
+      @include('frontend.themes.emporium.layouts.sections.home_right_iconbar')
+      @show
+
 {{-- For Include style files --}}
 @section('head')
     @parent
