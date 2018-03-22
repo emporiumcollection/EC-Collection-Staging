577d0755fc2c65ec8305c3355b538b711cf3e9e5
Ravinder Singh <I>
2018-03-22 16:23:49 +0530

    updated

--- resources/views/frontend/themes/emporium/pages/index.blade.php
+++ resources/views/frontend/themes/emporium/pages/index.blade.php
@@ -58,7 +58,7 @@
       @section('right_side_iconbar')
       
       @include('frontend.themes.emporium.layouts.sections.home_right_iconbar')
-      @show
+      @endsection
 
 {{-- For Include style files --}}
 @section('head')
