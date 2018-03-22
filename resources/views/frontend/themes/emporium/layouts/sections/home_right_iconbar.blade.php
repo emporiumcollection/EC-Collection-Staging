25a37ca99070e45394990af2eaf9472dfd43e2c4
Ravinder Singh <ravinder@dheerajtech.com>
2018-03-22 11:59:43 +0530

    added common sidebar

---
+++ resources/views/frontend/themes/emporium/layouts/sections/home_right_iconbar.blade.php
@@ -0,0 +1,27 @@
+<div class="homerightmenu sidefixednav">
+	<ul>
+    	<li class="hamburguricon">
+        	<div class="block-content togglenav content">
+            <span></span>
+            <span> </span>
+            <span></span>
+            </div>
+        </li>
+    	<li class="loginSignPopupButton">
+        	<a href="javascript:void(0)" class="clicktologin" style="display:none;"><i class="fa fa-lock"></i></a>
+            <a href="javascript:void(0)" class="login-user"><i class="fa fa-user"></i></a>
+            <ul class="user-setting">
+            	<li><a href="javascript:void(0)">Profile</a></li>
+                <li><a href="javascript:void(0)">Change Possword</a></li>
+                <li><a href="javascript:void(0)">Logout</a></li>
+            </ul>
+        </li>
+        <li class="calander"><a href="javascript:void(0)"><i class="fa fa-calendar"></i></a></li>
+    </ul>
+</div>
+<div class="homerightmenu contactfixed">
+    <ul>
+        <li><a href="tel:+19344511317"><i class="fa fa-phone"></i> +1 934 451 1317</a></li>
+        <li><a href="mailto: reservations@emporium-voyage.com"><i class="fa fa-envelope"></i> reservations@emporium-voyage.com</a></li>
+    </ul>
+</div>
---
+++ resources/views/frontend/themes/emporium/layouts/sections/pdp_right_iconbar.blade.php
@@ -0,0 +1,35 @@
+<div class="sidefixednav pdprightnav">
+	<ul>
+    	<li class="hamburguricon">
+        	<div class="block-content togglenav content">
+            <span></span>
+            <span> </span>
+            <span></span>
+            </div>
+        </li>
+    	<li class="loginSignPopupButton">
+        	<a href="javascript:void(0)" class="clicktologin"><i class="fa fa-lock"></i></a>
+            <a href="javascript:void(0)" class="login-user" style="display:none;"><i class="fa fa-user"></i></a>
+            <ul class="user-setting">
+            	<li><a href="javascript:void(0)">Profile</a></li>
+                <li><a href="javascript:void(0)">Change Possword</a></li>
+                <li><a href="javascript:void(0)">Logout</a></li>
+            </ul>
+        </li>
+        <li class="calander"><a href="javascript:void(0)"><i class="fa fa-calendar"></i></a></li>
+        <li><a href="javascript:void(0)"><img src="images/editorial-left-arrow.png" alt=""/></a></li>
+        <li><a href="javascript:void(0)"><img src="images/editorial-right-arrow.png" alt=""/></a></li>
+    </ul>
+</div>
+<div class="pdprightnav contactfixed">
+    <ul>
+        <li><a href="tel:+19344511317"><i class="fa fa-phone"></i> +1 934 451 1317</a></li>
+        <li><a href="mailto: reservations@emporium-voyage.com"><i class="fa fa-envelope"></i> reservations@emporium-voyage.com</a></li>
+    </ul>
+</div>
+<div class="pdprightnav backcontrol">
+	<ul>
+	    <li><a href="javascript:void(0)">BACK</a></li>
+    	<li><a href="javascript:void(0)" id="back-to-top">TOP</a></li>
+    </ul>
+</div>
