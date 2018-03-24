3d735036d6e2026e6af1cc8220067600f6f422a6
Ravinder Singh <I>
2018-03-24 18:54:25 +0530

    fixed exprience

--- public/themes/emporium/js/custom/destination.js
+++ public/themes/emporium/js/custom/destination.js
@@ -1,44 +1,96 @@
-$(document).ready(function() {
+$(document).ready(function () {
     /*
-     * For Select Collection of Left Sidebar
+     * For Select Destination of Left Sidebar
      */
     $(document).on('click', '[data-action="select-destination"]', function () {
         hideAllOption();
         var datObj = {};
         datObj.catID = 0;
-        if($(this).attr('data-id')>0){
+        if ($(this).attr('data-id') > 0) {
             datObj.catID = $(this).attr('data-id');
         }
         var params = $.extend({}, doAjax_params_default);
-        params['url'] = BaseURL+'/destination/destinatinos-ajax';
+        params['url'] = BaseURL + '/destination/destinatinos-ajax';
         params['data'] = datObj;
         params['successCallbackFunction'] = renderDestination;
         doAjax(params);
 
     });
 
-});
+    /*
+     * For Select Experience of Left Sidebar
+     */
+    $(document).on('click', '[data-action="select-experience"]', function () {
+        hideAllOption();
 
-function renderDestination(dataObj){
+        var params = $.extend({}, doAjax_params_default);
+        params['url'] = BaseURL + '/destination/experiences-ajax';
+        params['successCallbackFunction'] = renderExperience;
+        doAjax(params);
+
+    });
+
+});
+/*
+ * For Get Response of Destination Ajax
+ */
+function renderDestination(dataObj) {
 
     var data = {};
     data.main_title = 'Select Your Destination';
     data.sub_title = 'Home';
     data.id = 0;
-    if(dataObj.current_category!=undefined){
+    if (dataObj.current_category != undefined) {
         data.main_title = 'Home';
-        data.sub_title =  dataObj.current_category.category_name;
+        data.sub_title = dataObj.current_category.category_name;
         data.id = dataObj.current_category.id;
     }
 
     console.log(data);
     putDataOnLeft(data)
-    var destinationHtml='';
-    $(dataObj.dests).each(function(i,val){
-        destinationHtml += '<li><a class="cursor" data-action="select-destination" data-id="'+val.id+'">'+val.category_name+'</a><a></a></li>'
+    var destinationHtml = '';
+    $(dataObj.dests).each(function (i, val) {
+        destinationHtml += '<li><a class="cursor" data-action="select-destination" data-id="' + val.id + '">' + val.category_name + '</a><a></a></li>'
     });
 
     $('[data-option="selected-option-list"]').html(destinationHtml);
+    $('[data-option="global"]').removeClass('hide');
+    $('[data-option="child-global"]').removeClass('hide');
+    $('[data-option="selected-option-list"]').removeClass('hide');
+}
+
+
+/*
+ * For Get Response of Experience Ajax
+ */
+function renderExperience(dataObj) {
+
+    var data = {};
+    data.main_title = 'Select Your Experience';
+    data.sub_title = 'Home';
+    data.id = 0;
+    if (dataObj.current_category != undefined) {
+        data.main_title = 'Home';
+        data.sub_title = dataObj.current_category.category_name;
+        data.id = dataObj.current_category.id;
+    }
+
+    console.log(data);
+    putDataOnLeft(data)
+    var experienceHtml = '';
+    $(dataObj.dests).each(function (i, val) {
+        var imagePath = BaseURL+'/uploads/uploads/category_imgs/'+val.category_image;
+        if(val.category_image==''){
+            imagePath = BaseURL+'/themes/emporium/images/mountain-image.jpg';
+        }
+        experienceHtml += '<li><div class="navheadimage">';
+        experienceHtml += '<a href="javascript:void(0)">';
+        experienceHtml += '<img src="'+imagePath+'" alt="" class="mCS_img_loaded">';
+        experienceHtml += '<div class="headingoverlay">' + val.category_name + '</div>';
+        experienceHtml += '</a></div></li>';
+    });
+
+    $('[data-option="selected-option-list"]').html(experienceHtml);
     $('[data-option="global"]').removeClass('hide');
     $('[data-option="child-global"]').removeClass('hide');
     $('[data-option="selected-option-list"]').removeClass('hide');
