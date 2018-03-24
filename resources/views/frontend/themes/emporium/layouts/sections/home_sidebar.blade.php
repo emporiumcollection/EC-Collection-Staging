f51683a135914a1559bb5fec3096cfe1462e821c
Ravinder Singh <I>
2018-03-24 19:08:29 +0530

    fixed advertisement

--- resources/views/frontend/themes/emporium/layouts/sections/home_sidebar.blade.php
+++ resources/views/frontend/themes/emporium/layouts/sections/home_sidebar.blade.php
@@ -166,34 +166,33 @@
         </div>
         <div class="left-carousal">
             <div id="owl-carousel" class="owl-carousel">
+
                 <div class="item">
                     <div class="side-bar-why-book-with-us">
                         <div class="book-with-us-tittles">
                             <h2>Why book with us?</h2>
                         </div>
-                        <ul class="side-bar-book-with-us-list">
-                            <li>
-                                <h3>Handpicked Selection of Hotels</h3>
-                                <p>from selected luxury destinations worldwide</p>
-                            </li>
-                            <li>
-                                <h3>Upgrade and Late Checkout</h3>
-                                <p>At any Hotel upon Avilability</p>
-                            </li>
-                            <li>
-                                <h3>Preferred Guest Discounts at New Hotels</h3>
-                                <p>join our members club</p>
-                            </li>
-                            <li>
-                                <h3>Free Wifi</h3>
-                                <p>Guaranteed at all our Partner Hotels</p>
-                            </li>
-                        </ul>
+                        {{--*/ $uspmod = CommonHelper::getUspMod() /*--}}
+                        @if(!empty($uspmod['whybookwithus']))
+                            <ul class="side-bar-book-with-us-list">
+                                @foreach ($uspmod['whybookwithus'] as $usps)
+                                    <li>
+                                        <h3>{{$usps->title}}</h3>
+                                        <p>{{$usps->sub_title}}</p>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @endif
                     </div>
                 </div>
-                <div class="item">
-                    <a href="javascript:void(0)"><img src="images/left-side-banner.png"></a>
-                </div>
+                {{--*/ $sidebarads = CommonHelper::getSidebarAds('landing', 'Hotel') /*--}}
+                @if(!empty($sidebarads['leftsidebarads']))
+                    @foreach($sidebarads['leftsidebarads'] as $ads)
+                        <div class="item">
+                            <a href="{{ (strpos($ads->adv_link, 'http://') !== false) ? $ads->adv_link : 'http://'.$ads->adv_link }}"><img src="{{URL::to('uploads/users/advertisement/'.$ads->adv_img)}}"></a>
+                        </div>
+                    @endforeach
+                @endif
             </div>
         </div>
     </div>
