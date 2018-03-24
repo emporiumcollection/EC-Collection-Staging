8615650510a09e7bd7d049938f5d4554c1e70119
Puneet <puneet@number7even.de>
2018-03-24 13:42:12 +0530

    update

--- app/Helpers/CommonHelper.php
+++ app/Helpers/CommonHelper.php
@@ -56,7 +56,12 @@ class CommonHelper
 	static function getSidebarAds($pos='landing', $cat_id = ''){
 
         $data = array();
-        $data['leftsidebarads'] = \DB::table('tb_advertisement')->select('adv_img', 'adv_link')->where('adv_type', 'sidebar')->where('adv_status', 1)->where('adv_position', $pos)->where('ads_cat_id', $cat_id)->get();
+        $data['leftsidebarads'] = \DB::table('tb_advertisement')->select('adv_img', 'adv_link')->where('adv_type', 'sidebar')->where('adv_status', 1)->where('adv_position', $pos)
+		if($cat_id!='')
+		{
+			->where('ads_cat_id', $cat_id)
+		}
+		->get();
         return $data;
     }
 
