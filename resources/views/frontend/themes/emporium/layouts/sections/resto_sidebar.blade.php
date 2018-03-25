9d752cb7849264871cc82c6d72ed9af9cd12adcb
Puneet <puneet@number7even.de>
2018-03-25 18:56:02 +0530

    updated resto side bar

--- resources/views/frontend/themes/emporium/layouts/sections/resto_sidebar.blade.php
+++ resources/views/frontend/themes/emporium/layouts/sections/resto_sidebar.blade.php
@@ -1,7 +1,141 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: apple
- * Date: 21/03/18
- * Time: 7:53 PM
- */
+
+<div class="mobilemenu">
+    <div class="block-content togglenav content active mobilenavclosebtn">
+        <span></span>
+        <span> </span>
+        <span></span>
+    </div>
+    <div class="mobilemenu-inner">
+        <div class="mobilemainnav openmobilemenu">
+            <div class="mobilenavheader">
+                <a href="index.html"><img src="images/design-location-logo.png" alt="Emporium Voyage" class="img-responsive"/></a>
+                <a href="hotel-pdp.html" class="homelinknav backtodestinationnav"><i class="fa fa-angle-left"></i> BACK TO HAM YARD HOTEL</a>
+            </div>
+            <ul class="mobilemenulist">
+                <li>
+                    <div class="mobile-leftsearch">
+                        <input type="text" class="form-control" id="search"/>
+                        <button type="button"><i class="fa fa-search"></i></button>
+                    </div>
+                    <div id="filtersearchpopup" style="display:none;">
+                        <ul>
+                            <li>
+                                <a class="searchresultdata" href="javascript:void(0)"><i class="iconsheet icon-destinations"></i> Destinations <span>(3)</span></a>
+                            </li>
+                            <li>
+                                <a class="searchresultdata" href="javascript:void(0)"><i class="iconsheet icon-collections"></i> Collections <span>(11)</span></a>
+                            </li>
+                            <li>
+                                <a class="searchresultdata" href="javascript:void(0)"><i class="iconsheet icon-restaurant"></i> Restaurant <span>(5)</span></a>
+                            </li>
+                            <li>
+                                <a class="searchresultdata" href="javascript:void(0)"><i class="iconsheet icon-bar"></i> Bar <span>(2)</span></a>
+                            </li>
+                            <li>
+                                <a class="searchresultdata" href="javascript:void(0)"><i class="iconsheet icon-spa"></i> Spa <span>(2)</span></a>
+                            </li>
+                        </ul>
+                    </div>
+               </li>
+                <li><a href="#restaurant1" class="closenavlink scrollpage">RESTAURANT 1</a></li>
+                <li><a href="#restaurant2" class="closenavlink scrollpage">RESTAURANT 2</a></li>
+                <li><a href="#restaurant3" class="closenavlink scrollpage">RESTAURANT 3</a></li>
+                <li><a href="#bar1" class="closenavlink scrollpage">BAR 1</a></li>
+                <li><a href="#video" class="closenavlink scrollpage">VIDEO</a></li>
+                <li><a href="#seasonal-events" class="closenavlink scrollpage">SEASONAL EVENTS</a></li>
+                <li><a href="#instagram-gallery" class="closenavlink scrollpage">SPECIAL OCCASIONS</a></li>
+                <li><a href="#get-directions" class="closenavlink scrollpage">GET DIRECTIONS</a></li>
+                <li><a href="javascript:void(0)" class="yellowbtn">VISIT HOTEL</a></li>
+            </ul>
+            <div class="left-carousal">
+                <div id="owl-carousel" class="owl-carousel">
+                
+                <div class="item">
+                    <div class="side-bar-why-book-with-us">
+                        <div class="book-with-us-tittles">
+                            <h2>Why book with us?</h2>
+                        </div>
+                        <ul class="side-bar-book-with-us-list">
+                        <li>
+                            <h3>Handpicked Selection of Hotels</h3>
+                            <p>from selected luxury destinations worldwide</p>
+                        </li>
+                        <li>
+                            <h3>Upgrade and Late Checkout</h3>
+                            <p>At any Hotel upon Avilability</p>
+                        </li>
+                        <li>
+                            <h3>Preferred Guest Discounts at New Hotels</h3>
+                            <p>join our members club</p>
+                        </li>
+                        <li>
+                            <h3>Free Wifi</h3>
+                            <p>Guaranteed at all our Partner Hotels</p>
+                        </li>
+                     </ul>
+                    </div>
+                </div>
+                <div class="item">
+                    <a href="javascript:void(0)"><img src="images/left-side-banner.png"></a>
+                </div>
+              </div>
+            </div>
+            
+        </div>
+        <div class="restaurantnav">
+            <div class="mobilenavheader">
+                <h3>RESTAURANT 1</h3>
+                <a href="javascript:void(0)" class="homelinknav backtohomelink"><i class="fa fa-angle-left"></i> BACK</a>
+            </div>
+            <ul class="mobilemenulist">
+                <li>
+                    <div class="mobile-leftsearch">
+                        <input type="text" class="form-control"/>
+                        <button type="button"><i class="fa fa-search"></i></button>
+                    </div>
+               </li>
+               <li><a href="#restaurantSlider">INFO</a></li>
+               <li><a href="#bar">BAR 1</a></li>
+               <li><a href="#video" class="">VIDEO</a></li>
+               <li><a href="#seasonal-events">SEASONAL EVENTS</a></li>
+               <li><a href="#instagram-gallery">SPECIAL OCCASIONS</a></li>
+               <li><a href="#get-directions">GET DIRECTIONS</a></li>
+               <li><a href="visit-hotel.html" class="yellowbtn">VISIT HOTEL</a></li>
+            </ul>
+            <div class="left-carousal">
+                <div id="owl-carousel" class="owl-carousel">
+                
+                <div class="item">
+                    <div class="side-bar-why-book-with-us">
+                        <div class="book-with-us-tittles">
+                            <h2>Why book with us?</h2>
+                        </div>
+                        <ul class="side-bar-book-with-us-list">
+                        <li>
+                            <h3>Handpicked Selection of Hotels</h3>
+                            <p>from selected luxury destinations worldwide</p>
+                        </li>
+                        <li>
+                            <h3>Upgrade and Late Checkout</h3>
+                            <p>At any Hotel upon Avilability</p>
+                        </li>
+                        <li>
+                            <h3>Preferred Guest Discounts at New Hotels</h3>
+                            <p>join our members club</p>
+                        </li>
+                        <li>
+                            <h3>Free Wifi</h3>
+                            <p>Guaranteed at all our Partner Hotels</p>
+                        </li>
+                     </ul>
+                    </div>
+                </div>
+                <div class="item">
+                    <a href="javascript:void(0)"><img src="images/left-side-banner.png"></a>
+                </div>
+              </div>
+            </div>
+        </div>
+        
+    </div>
+</div>
