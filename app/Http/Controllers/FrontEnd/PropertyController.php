3c19bb646629fdcc8cb865eef1c9fadd55a80ec8
Puneet <puneet@number7even.de>
2018-04-09 13:17:46 +0530

    ajax query

--- app/Http/Controllers/FrontEnd/PropertyController.php
+++ app/Http/Controllers/FrontEnd/PropertyController.php
@@ -443,7 +443,7 @@ class PropertyController extends Controller {
 		$query .= $getPriceQry;
         //$query .= ", (SELECT pcrp.rack_rate FROM tb_properties_category_rooms_price pcrp  where pr.id=pcrp.property_id  order by pcrp.rack_rate DESC limit 0,1 ) as price " ;
         $query .= " FROM tb_properties pr ";
-        $whereClause =" WHERE ((pr.property_name LIKE '% ".$keyword."%'AND pr.property_type = 'Hotel') OR city LIKE '% ".$keyword."%' ".$catprops." ) AND pr.property_status = 1  ".$filterPriceQry;
+        $whereClause =" WHERE ((pr.property_name LIKE '% ".$keyword."%' AND pr.property_type = 'Hotel') OR city LIKE '% ".$keyword."%' ".$catprops." ) AND pr.property_status = 1  ".$filterPriceQry;
         $orderBy = "ORDER BY (SELECT rack_rate FROM tb_properties_category_rooms_price pcrp WHERE pcrp.property_id = pr.id ORDER BY rack_rate DESC LIMIT 1) * 1 DESC, pr.editor_choice_property desc, pr.feature_property desc ";
         $limit = " LIMIT ". $pageStart.",".$perPage;
         $finalQry = $query.$whereClause.$orderBy.$limit ;
