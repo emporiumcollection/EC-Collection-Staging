9d92b84813a63e5a44cea510948f59b60b05879c
Ravinder Singh <I>
2018-04-11 21:40:13 +0530

    updated

--- app/Http/Controllers/SpaController.php
+++ app/Http/Controllers/SpaController.php
@@ -38,7 +38,7 @@ class SpaController extends Controller {
         /*\DB::table('tb_module')
             ->where('module_id', '=',$row->module_id )
             ->update(array('module_config' => \SiteHelpers::CF_encode_json($config)));*/
-        $row = \DB::table('tb_module')->where('module_id', 99)->first();
+        /*$row = \DB::table('tb_module')->where('module_id', 99)->first();
         $data = \SiteHelpers::CF_decode_json($row->module_config);
         $data['grid'][3]['field'] = 'description';
         $data['grid'][3]['label'] = 'Description';
@@ -51,7 +51,7 @@ class SpaController extends Controller {
         \DB::table('tb_module')
             ->where('module_id', '=',99 )
             ->update(array('module_config' => \SiteHelpers::CF_encode_json($data)));
-        //die;
+        //die;*/
         if($this->access['is_view'] ==0)
 			return Redirect::to('dashboard')
 				->with('messagetext', \Lang::get('core.note_restric'))->with('msgstatus','error');
