7bbaa315f0c07745593c0c1490dee1d32226e205
Puneet <puneet@number7even.de>
2018-04-16 15:58:54 +0530

    sort num default

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -45,8 +45,8 @@ class SliderController extends Controller {
 			return Redirect::to('dashboard')
 				->with('messagetext', \Lang::get('core.note_restric'))->with('msgstatus','error');
 
-		$sort = (!is_null($request->input('sort')) ? $request->input('sort') : 'updated');
-		$order = (!is_null($request->input('order')) ? $request->input('order') : 'asc');
+		$sort = (!is_null($request->input('sort')) ? $request->input('sort') : 'sort_num');
+		$order = (!is_null($request->input('order')) ? $request->input('order') : 'desc');
 		// End Filter sort and order for query 
 		// Filter Search for query		
 		$filter = (!is_null($request->input('search')) ? $this->buildSearch() : '');
@@ -176,6 +176,14 @@ class SliderController extends Controller {
 		
 		$this->data['id'] = $id;
 		$this->data['access']		= $this->access;
+		
+		$checkslid = \DB::table('tb_sliders')->get();
+		$s=1;
+		foreach($checkslid as $slid)
+		{
+			\DB::table('tb_sliders')->where('id', $slid->id)->update(['sort_num'=>$s]);
+			$s++;
+		}
 		return view('slider.view',$this->data);	
 	}	
 
