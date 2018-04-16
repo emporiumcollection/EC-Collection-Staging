42957040f85fc3840092b49c4f947a4974a5eb04
Ravinder Singh <I>
2018-04-16 12:51:51 +0530

    Made common global search bar

---
+++ resources/views/frontend/themes/emporium/layouts/sections/global-search-list.blade.php
@@ -0,0 +1,10 @@
+<ul class="mobilemenulist hide" data-option="dest-option-list">
+</ul>
+<ul class="mobilemenulist hide" data-option="collection-option-list">
+</ul>
+<ul class="mobilemenulist hide" data-option="resto-option-list">
+</ul>
+<ul class="mobilemenulist hide" data-option="bar-option-list">
+</ul>
+<ul class="mobilemenulist hide" data-option="spa-option-list">
+</ul>
