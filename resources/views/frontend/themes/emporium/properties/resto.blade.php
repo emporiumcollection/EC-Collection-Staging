0820f86ae8c9b83dd8cd6be4daab13584132e1e0
Puneet <puneet@number7even.de>
2018-04-19 20:42:39 +0530

    active class

--- resources/views/frontend/themes/emporium/properties/resto.blade.php
+++ resources/views/frontend/themes/emporium/properties/resto.blade.php
@@ -18,7 +18,7 @@
 			  @if(!empty($resturantArr))
 				  @foreach($resturantArr as $resturant)
 					@if(array_key_exists('dataslider',$resturant))
-						@if($clsact=='') {{--*/ $clsact = 'active'; /*--}} @endif
+						{{--*/ $clsact = ($clsact!='active') ? 'active' : ''; /*--}}
 					  <div class="item {{$clsact}}" style="background:url('{{$resturant->dataslider}}') center center no-repeat; background-size:cover;">
 						<div class="carousel-caption">
 						  <h1>{{$resturant->title}}</h1>
@@ -33,7 +33,7 @@
 			  @if(!empty($barsArr))
 				  @foreach($barsArr as $bars)
 					@if(array_key_exists('dataslider',$bars))
-						@if($clsact=='') {{--*/ $clsact = 'active'; /*--}} @endif
+						{{--*/ $clsact = ($clsact!='active') ? 'active' : ''; /*--}}
 					  <div class="item {{$clsact}}" style="background:url('{{$bars->dataslider}}') center center no-repeat; background-size:cover;">
 						<div class="carousel-caption">
 						  <h1>{{$bars->title}}</h1>
@@ -52,7 +52,7 @@
 			  @if(!empty($spasArr))
 				  @foreach($spasArr as $spas)
 					@if(array_key_exists('dataslider',$spas))
-						@if($clsact=='') {{--*/ $clsact = 'active'; /*--}} @endif
+						{{--*/ $clsact = ($clsact!='active') ? 'active' : ''; /*--}}
 					  <div class="item {{$clsact}}" style="background:url('{{$spas->dataslider}}') center center no-repeat; background-size:cover;">
 						<div class="carousel-caption">
 						  <h1>{{$spas->title}}</h1>
