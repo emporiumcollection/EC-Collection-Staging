44404fa12f85271381282a1dd125c81b03adb357
Puneet <puneet@number7even.de>
2018-04-19 18:52:59 +0530

    updated bar

--- resources/views/frontend/themes/emporium/layouts/sections/spa_detail_sidebar.blade.php
+++ resources/views/frontend/themes/emporium/layouts/sections/spa_detail_sidebar.blade.php
@@ -6,7 +6,7 @@
         <span></span>
     </div>
     <div class="mobilemenu-inner">
-        <div class="restaurantnav">
+        <div class="mobilemainnav openmobilemenu">
             <div class="mobilenavheader">
 				<a href="{{URL::to('')}}"><img src="{{ asset('themes/emporium/images/logo.png') }}" alt="Emporium Voyage" class="img-responsive"/></a>
                 @if(!empty($spasArr[0])) <h3>{{$spasArr[0]->title}}</h3> @endif
