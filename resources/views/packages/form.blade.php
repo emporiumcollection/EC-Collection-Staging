8e0533fa4ba2584822ff264a042b6eecc9155564
Puneet <puneet@number7even.de>
2018-04-19 12:19:03 +0530

    existing mods selected

--- resources/views/packages/form.blade.php
+++ resources/views/packages/form.blade.php
@@ -146,8 +146,9 @@
 									<div class="col-md-6">
 									  <select name='package_modules[]' multiple class='select2'>
 										@if(!empty($mods))
+										{{--*/ $existmod = explode(',',$row['package_modules']); /*--}}
 											@foreach($mods as $mod)
-												<option value="{{$mod->module_id}}">{{$mod->module_title}}</option>
+												<option value="{{$mod->module_id}}" {{(array_key_exists($mod->module_id, $existmod)) ? 'selected="selected"' : '' }} >{{$mod->module_title}}</option>
 											@endforeach
 										@endif
 									  </select> 
