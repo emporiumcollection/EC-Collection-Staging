c7107974f9892c4d2e8734c523d3eae7cd0ddbb9
Puneet <puneet@number7even.de>
2018-04-30 19:33:34 +0530

    testing

--- app/Http/Controllers/FrontEnd/DestinationController.php
+++ app/Http/Controllers/FrontEnd/DestinationController.php
@@ -285,7 +285,7 @@ class DestinationController extends Controller {
 	{
 		$res = array();
 		$type = $request->type;
-		$city = $request->city;
+		echo $city = $request->city;
 		if($type!='' && $city!='')
 		{
 			$srchtbl = 'tb_restaurants';
@@ -298,6 +298,7 @@ class DestinationController extends Controller {
 				$srchtbl = 'tb_spas';
 			}
 			$checkcatid = \DB::table('tb_categories')->select('id')->where('category_name', $city)->first();
+			print_r($checkcatid);
 			if(!empty($checkcatid))
 			{
 				$searchtable = \DB::table($srchtbl)->select('id','title','alias')->where('category_id', $checkcatid->id)->get();
