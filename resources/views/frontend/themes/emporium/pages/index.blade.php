0885a8b97bdeb313f92366fdb0ec291cbf3d630d
tarun-vc <33755164+tarun-vc@users.noreply.github.com>
2018-05-30 18:16:54 +0530

    Update index.blade.php

--- resources/views/frontend/themes/emporium/pages/index.blade.php
+++ resources/views/frontend/themes/emporium/pages/index.blade.php
@@ -16,7 +16,7 @@
                     @foreach($slider as $key => $slider_row)
                       <div class="item {{($key == 0)? 'active' : ''}}">
 		      <div class="image-overaly-bg"></div>
-                         <a><img src="{{url('uploads/slider_images/'.$slider_row->slider_img)}}" alt="{{$slider_row->slider_title}}"></a>
+                         <a href="{{$slider_row->slider_link}}"><img src="{{url('uploads/slider_images/'.$slider_row->slider_img)}}" alt="{{$slider_row->slider_title}}"></a>
                          <div class="carousel-caption">
                             <h1><a href="{{$slider_row->slider_link}}">{{$slider_row->slider_title}}</a></h1>
                             <p><a href="{{$slider_row->slider_link}}" style="color:white;">{{$slider_row->slider_description}}</a></p>
