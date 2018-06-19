b2fc16e8a49f6a8ed9a3a971c2114b7cc8af71f2
number7even-dev <aman@number7even.de>
2018-06-19 18:10:45 +0530

    test

--- app/Http/Controllers/Frontend/PropertyController.php
+++ app/Http/Controllers/Frontend/PropertyController.php
@@ -257,7 +257,7 @@ class PropertyController extends Controller {
         $childIds = json_decode($request->childid);
         $departure = '';
         $keyword = trim($request->keywordstr);
-        
+        echo $childIds.count($childIds);
         $chldIds = array();
 		if(count($chldIds) > 0) {
 			
