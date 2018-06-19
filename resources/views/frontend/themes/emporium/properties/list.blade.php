e325f2bfa38b6b34b31e29c4a27b6b72cc82340d
number7even-dev <aman@number7even.de>
2018-06-19 18:05:37 +0530

    test

--- resources/views/frontend/themes/emporium/properties/list.blade.php
+++ resources/views/frontend/themes/emporium/properties/list.blade.php
@@ -447,7 +447,7 @@ $('input[name="departure"]').daterangepicker({
 function rad_rungridAjax(){
     $.ajax({
         type: 'POST',
-        data: {childid:'{{ $child_ids }}'},
+        data: {keywordstr:'{{ slug }}',childid:'{{ $child_ids }}'},
         url: '{{ URL::to('properties-grid-ajax') }}',
         success: function(data){
             console.log(data);
