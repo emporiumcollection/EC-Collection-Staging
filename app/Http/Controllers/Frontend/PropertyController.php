6557c0e239e32348904e59952ce79b1714be681c
number7even-dev <aman@number7even.de>
2018-06-20 14:53:12 +0530

    changes

--- app/Http/Controllers/Frontend/PropertyController.php
+++ app/Http/Controllers/Frontend/PropertyController.php
@@ -155,7 +155,44 @@ class PropertyController extends Controller {
         if(count($getcatsID) > 0){
             $timplod = implode(',',$getcatsID);
             $catprops = " OR pr.id in(".$timplod.") ";
-        }
+        }        
+        
+		if (!empty($cateObj)) {
+			$channel_url = $cateObj->category_youtube_channel_url;
+			$this->data['channel_url'] = $channel_url;
+			/*$cateObjtemp = \DB::table('tb_categories')->where('parent_category_id', $cateObj->id)->where('category_published', 1)->get();
+			if (!empty($cateObjtemp)) {
+				$chldIds = $this->fetchcategoryChildListIds($cateObj->id);
+				array_unshift($chldIds, $cateObj->id);
+			} else {
+				$chldIds[] = $cateObj->id;
+			}*/
+            $chldIds = $this->fetchcategoryChildListIds($cateObj->id);
+            if(count($chldIds) <= 0){ $chldIds[] = $cateObj->id; }
+			$getcats = '';
+			if (!empty($chldIds)) {
+				$getcats = " AND (" . implode(" || ", array_map(function($v) {
+									return sprintf("FIND_IN_SET('%s', property_category_id)", $v);
+								}, array_values($chldIds))) . ")";
+			}
+
+			if ($arrive != '') {
+				$getcats = '';
+				if (!empty($chldIds)) {
+					$getcats = " AND (" . implode(" || ", array_map(function($v) {
+										return sprintf("FIND_IN_SET('%s', pr.property_category_id)", $v);
+									}, array_values($chldIds))) . ")";
+				}
+				if ($departure != '') {
+					$getdestind = " AND pctr.room_active_to <= '$departure'";
+				}
+				$catprops = " OR pr.id in( SELECT pr.id FROM tb_properties pr, tb_properties_category_rooms pctr   WHERE pctr.property_id = pr.id AND  pr.property_status='1' AND pctr.room_active_from <= '".$arrive."' ".$getdestind."  ".$getcats." ) ";
+			} else {
+				$catprops = " OR pr.id in(SELECT id FROM tb_properties WHERE property_status='1' $getcats ) ";
+			}
+
+		}
+
 		
 		$perPage = 42;
 		$pageNumber = 1;
