1de1301b823a5276863ba6aad459d25e38def63e
number7even-dev <aman@number7even.de>
2018-06-20 13:50:17 +0530

    test query

--- app/Http/Controllers/Frontend/PropertyController.php
+++ app/Http/Controllers/Frontend/PropertyController.php
@@ -169,12 +169,18 @@ class PropertyController extends Controller {
 		$query = "SELECT pr.editor_choice_property,pr.property_usp,pr.feature_property,pr.id,pr.property_name,pr.property_slug,pr.property_category_id ";
 		$query .= ", (SELECT pcrp.rack_rate FROM tb_properties_category_rooms_price pcrp  where pr.id=pcrp.property_id  order by pcrp.rack_rate DESC limit 0,1 ) as price " ;
 		$query .= " FROM tb_properties pr ";
+        
+        $query1 = "SELECT pr.editor_choice_property,pr.property_usp,pr.feature_property,pr.id,pr.property_name,pr.property_slug,pr.property_category_id, pcrp.rack_rate as price";
+		$query1 .= " FROM tb_properties pr LEFT JOIN tb_properties_category_rooms_price pcrp ON pr.id = pcrp.property_id";
+        
 		$whereClause =" WHERE ((pr.property_name LIKE '%".$keyword."%' AND pr.property_type = 'Hotel') OR city LIKE '%".$keyword."%' ".$catprops." ) AND pr.property_status = 1 AND  pr.feature_property = 0 ";
         $whereClause1 =" WHERE ((pr.property_name LIKE '%".$keyword."%' AND pr.property_type = 'Hotel') OR city LIKE '%".$keyword."%' ".$catprops1." ) AND pr.property_status = 1 AND  pr.feature_property = 0 ";
 		$orderBy = "ORDER BY (SELECT rack_rate FROM tb_properties_category_rooms_price pcrp WHERE pcrp.property_id = pr.id ORDER BY rack_rate DESC LIMIT 1) * 1 DESC, pr.editor_choice_property desc  ";
+        
+        $orderBy1 = "ORDER BY pcrp.rack_rate DESC, pr.editor_choice_property desc  ";
 		$limit = " LIMIT ". $pageStart.",".$perPage; 
 		$finalQry = $query.$whereClause.$orderBy.$limit ; 
-        $finalQry1 = $query.$whereClause1.$orderBy.$limit ; 
+        $finalQry1 = $query1.$whereClause1.$orderBy1.$limit ; 
 		$CountRecordQry = "Select count(*) as total_record from tb_properties pr ".$whereClause ;
 			
 			//Feature Query
