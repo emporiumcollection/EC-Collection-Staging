0ad97925374901f08d42f671507fe709217db557
number7even-dev <aman@number7even.de>
2018-06-25 18:07:02 +0530

    changes

--- resources/views/users_admin/metronic/dashboard.blade.php
+++ resources/views/users_admin/metronic/dashboard.blade.php
@@ -1,5 +1,18 @@
 @extends('users_admin.metronic.layouts.app')
 
+@section('page_name')
+    Dashboard
+@stop
+
+@section('breadcrumb')
+    <li class="m-nav__separator"> - </li>
+    <li class="m-nav__item"> 
+        <a href="{{ URL::to('dashboard')}}" class="m-nav__link"> 
+            <span class="m-nav__link-text"> Dashboard </span> 
+        </a> 
+    </li>
+@stop
+
 @section('content')
     Dashboard Content
 @stop
