35f2d8981ae96128e4eb2c45438ea1a0a4134c16
number7even-dev <aman@number7even.de>
2018-06-25 17:42:12 +0530

    changes

--- resources/views/users_admin/metronic/layouts/app.blade.php
+++ resources/views/users_admin/metronic/layouts/app.blade.php
@@ -68,14 +68,14 @@
 						</div>
 						<!-- END: Brand -->
 						<div class="m-stack__item m-stack__item--fluid m-header-head" id="m_header_nav">
-							<div class="m-header__title">
+							<div class="m-header__title" style="display: none;">
 								<h3 class="m-header__title-text"> Applications </h3>
 							</div>
 							<!-- BEGIN: Horizontal Menu -->
 							<button class="m-aside-header-menu-mobile-close  m-aside-header-menu-mobile-close--skin-light " id="m_aside_header_menu_mobile_close_btn">
 								<i class="la la-close"></i>
 							</button>
-							<div id="m_header_menu" class="m-header-menu m-aside-header-menu-mobile m-aside-header-menu-mobile--offcanvas  m-header-menu--skin-light m-header-menu--submenu-skin-light m-aside-header-menu-mobile--skin-light m-aside-header-menu-mobile--submenu-skin-light "  >
+							<div id="m_header_menu" class="m-header-menu m-aside-header-menu-mobile m-aside-header-menu-mobile--offcanvas  m-header-menu--skin-light m-header-menu--submenu-skin-light m-aside-header-menu-mobile--skin-light m-aside-header-menu-mobile--submenu-skin-light " style="display: none;"  >
 								<ul class="m-menu__nav  m-menu__nav--submenu-arrow ">
 									<li class="m-menu__item  m-menu__item--submenu m-menu__item--rel"  m-menu-submenu-toggle="click" aria-haspopup="true">
 										<a  href="javascript:;" class="m-menu__link m-menu__toggle">
