19f0b04253c0310519250b26e74aedf4c179b21b
number7even-dev <aman@number7even.de>
2018-10-22 15:29:40 +0530

    Revert "temp handler"
    This reverts commit 805769120c28e8c9c41a1e83cf06469a7a6f41c6.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,9 +3,6 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -16,8 +13,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        HttpException::class,
-        ModelNotFoundException::class,
+        \Symfony\Component\HttpKernel\Exception\HttpException::class,
     ];
 
     /**
@@ -30,6 +26,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $e)
     {
+        if ($this->shouldReport($e)) {
+            $airbrakeNotifier = \App::make('Airbrake\Notifier');
+            $airbrakeNotifier->notify($e);
+        }
         return parent::report($e);
     }
 
@@ -42,10 +42,8 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if ($e instanceof ModelNotFoundException) {
-            $e = new NotFoundHttpException($e->getMessage(), $e);
-        }
-
         return parent::render($request, $e);
+		
+		//return response()->view('errors.404', [], 500);
     }
 }
