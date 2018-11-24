9c38a6e54c7a916d595184e7a71ad895ebefc500
riaankleynhans <riaan@number7even.com>
2018-11-24 14:56:19 +0100

    Updated hotel-property.php
    Updates Support cases and text

--- resources/lang/Deutsch/hotel-property.php
+++ resources/lang/Deutsch/hotel-property.php
@@ -1,45 +1,45 @@
 <?php 
 return array( 
     "type-heading"			  => "Add Category",
-    "type-info"			      => "Hotel Property type info",
-    "room-heading"			  => "Add Room",
-    "room-info"			      => "Room info",
-    "room-detail-info"			      => "Room detail info",
-    "room-image-info"			      => "Room image info",
-    "room-amenties-info"			      => "Room image info",
-    "season-heading"			  => "Add Season",
-    "season-info"			      => "Hotel season info",
+    "type-info"			      => "In this section we add all the Property Room Categories. We only sell your High-end Rooms, so ensure to only ad those categories. consult the documentation if you require detailed information.",
+    "room-heading"			  => "Add Suites",
+    "room-info"			      => "Now that we added our property suite categories, we will assign the suites to the respective category. Presedential Suite * 2 ",
+    "room-detail-info"			      => "This section is user to descrie your suite, names & availability.Choose full year to ensure full reservation ability.",
+    "room-image-info"			      => "Upload your suite images. Be sure to include high quality, non pixelated images for the best member experience. Quality is king. Show us your best images for revision.",
+    "room-amenties-info"			      => "Define your suite Amenities. Multi-Select your Suite Amenities and save to update.",
+    "season-heading"			  => "Add Seasons",
+    "season-info"			      => "Setup your Suite Seasons. Step1. Create the seasons. Step 2. Set the from and to sales dates.",
     
     "add-season-info"			      => "add season info",
     "add-date-info"			      => "add date info",
     
-    "price-heading"			  => "Add Price",
-    "price-info"			      => "Hotel price info",
+    "price-heading"			  => "Add Suite Price",
+    "price-info"			      => "Add your Suite Price by previously defined season. Select the season for which you wish to add a Price. Make sure to add the rack rate, single price and extra (Adult, Junior or Baby) adjust by adding daily rates where applicable.",
     
-    "image-heading"			  => "Add Image",
-    "image-info"			      => "Hotel image info",
+    "image-heading"			  => "Add Property Images",
+    "image-info"			      => "Make sure to add apprpriate images that sell your main USP's",
     
-    "image-slider-heading"			  => "Add Image slider",
-    "image-slider-info"			      => "Hotel image slider info",
+    "image-slider-heading"			  => "Add Slider Images",
+    "image-slider-info"			      => "This is your opening slider. Make sure to choose your top selling USP images. Use max 7 images to ensure optimal viewing expereince.",
     
-    "hotel-brochure-heading"			  => "Add Hotel Brochure",
-    "hotel-brochure-info"			      => "Hotel Brochure info",
+    "hotel-brochure-heading"			  => "Add Property Brochure",
+    "hotel-brochure-info"			      => "Include your Hotel brochure to share with members or Property agents to increase sales",
     
-    "hotel-menu-heading"			  => "Add Hotel Menu",
-    "hotel-menu-info"			      => "Hotel Menu info",
+    "hotel-menu-heading"			  => "Add Property Restaurant Menu's",
+    "hotel-menu-info"			      => "Add your Property Restaurant menus. These can be updated at any point and adds value to your sales & promotion.",
     
-    "spa-brochure-heading"			  => "Add Spa Brochure",
-    "spa-brochure-info"			      => "Spa Brochure info",
+    "spa-brochure-heading"			  => "Add Property Spa Brochure",
+    "spa-brochure-info"			      => "Include your property Spa Brochure to enhance sales via the Spa Module",
     
-    "contract-heading"			  => "Add Contarct",
-    "contract-info"			      => "Contract info",
+    "contract-heading"			  => "Property Contracts",
+    "contract-info"			      => "This section hold all the relevant contract info for contractual transactions",
     
-    "spa-gallery-heading"			  => "Add Spa Gallery",
-    "spa-gallery-info"			      => "Spa Gallery info",
+    "spa-gallery-heading"			  => "Add Spa Gallery Images",
+    "spa-gallery-info"			      => "This section holds all your Spa images, ensure to include your best USP sales images. Ensure you uplaoded your Spa brochure where available.",
     
     "restaurant-gallery-heading"			  => "Add Restaurant Gallery",
-    "restaurant-gallery-info"			      => "Restaurant Gallery info",
+    "restaurant-gallery-info"			      => "This section holds all your Restaurant images, ensure to include your best USP sales images. Ensure you uplaoded your Restaurant brochures where available.",
     
     "bar-gallery-heading"			  => "Add Bar Gallery",
-    "bar-gallery-info"			      => "Bar Gallery info",
+    "bar-gallery-info"			      => "This section holds all your bar images, ensure to include your best USP sales images. Ensure you uplaoded your Bar brochure where available.",
 );
