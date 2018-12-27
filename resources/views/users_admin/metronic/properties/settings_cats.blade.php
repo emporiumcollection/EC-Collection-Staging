3e16e3bdc6651862fbb7541afd59fa5bfe31df39
riaankleynhans <riaan@number7even.com>
2018-12-27 12:33:51 +0100

    Update settings_cats.blade.php
    Suite.. Urls...

--- resources/views/users_admin/metronic/properties/settings_cats.blade.php
+++ resources/views/users_admin/metronic/properties/settings_cats.blade.php
@@ -80,96 +80,95 @@
 											<ul class="m-nav">
 												<li class="m-nav__section m-nav__section--first">
 													<span class="m-nav__section-text">
-														Quick Actions
-													</span>
-												</li> 
-                                                <li class="m-nav__item">
-													<a href="http://localhost:8181/emporium-staging-forge/public/properties/update/2309?return=" class="m-nav__link">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Hotel/Property
-														</span>
-													</a>
-											    </li> 
-                                                <li class="m-nav__item">
-													<a href="http://localhost:8181/emporium-staging-forge/public/properties_settings/2309/types" class="m-nav__link">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Room Types
-														</span>
-													</a>
-											    </li> 
-                                                <li class="m-nav__item">
-													<a href="http://localhost:8181/emporium-staging-forge/public/properties_settings/2309/rooms" class="m-nav__link">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Rooms
-														</span>
-													</a>
-											    </li> 
-                                                <li class="m-nav__item">
-													<a href="http://localhost:8181/emporium-staging-forge/public/properties_settings/2309/seasons" class="m-nav__link">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Seasons
-														</span>
-													</a>
-											    </li> 
-                                                <li class="m-nav__item">
-													<a href="http://localhost:8181/emporium-staging-forge/public/properties_settings/2309/calendar" class="m-nav__link">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Reservation Management
-														</span>
-													</a>
-											    </li> 
-                                                <li class="m-nav__item">
-													<a href="http://localhost:8181/emporium-staging-forge/public/properties_settings/2309/price" class="m-nav__link">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Price
-														</span>
-													</a>
-											    </li> 
-                                                <li class="m-nav__item">
-													<a href="http://localhost:8181/emporium-staging-forge/public/properties_settings/2309/property_documents" class="m-nav__link">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Property Documents
-														</span>
-													</a>
-											    </li> 
-                                                <li class="m-nav__item">
-													<a href="http://localhost:8181/emporium-staging-forge/public/properties_settings/2309/property_images" class="m-nav__link">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Images
-														</span>
-													</a>
-											    </li> 
-                                                <li class="m-nav__item">
-													<a href="http://localhost:8181/emporium-staging-forge/public/properties_settings/2309/gallery_images" class="m-nav__link">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Galleries
-														</span>
-													</a>
-											    </li> 
-                                                <li class="m-nav__item">
-													<a href="http://localhost:8181/emporium-staging-forge/public/advertising" class="m-nav__link">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Become Featured
-														</span>
-													</a>
-											    </li> 
-                                                <li class="m-nav__item">
-													<a href="https://emporium-collection.com/" class="m-nav__link" target="_blank">
-														<i class="m-nav__link-icon"></i>
-														<span class="m-nav__link-text">
-															Get Help
-														</span>
-													</a>
+                                                                    Quick Actions
+																</span>
+															</li> 
+                                                            <li class="m-nav__item">
+																<a href="{{ URL::to('properties/update/'.$row->id.'?return='.$return) }}" class="m-nav__link">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Hotel/Property
+																	</span>
+																</a>
+														    </li> 
+                                                            <li class="m-nav__item">
+																<a href="{{ URL::to('properties_settings/'.$row->id.'/types')}}" class="m-nav__link">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Suite Types
+																	</span>
+																</a>
+														    </li> 
+                                                            <li class="m-nav__item">
+																<a href="{{ URL::to('properties_settings/'.$row->id.'/rooms')}}" class="m-nav__link">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Suites
+																	</span>
+																</a>
+														    </li> 
+                                                            <li class="m-nav__item">
+																<a href="{{ URL::to('properties_settings/'.$row->id.'/seasons')}}" class="m-nav__link">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Seasons
+																	</span>
+																</a>
+														    </li> 
+                                                            <li class="m-nav__item">
+																<a href="{{ URL::to('properties_settings/'.$row->id.'/calendar')}}" class="m-nav__link">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Reservation Management
+																	</span>
+																</a>
+														    </li> 
+                                                            <li class="m-nav__item">
+																<a href="{{ URL::to('properties_settings/'.$row->id.'/price')}}" class="m-nav__link">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Price
+																	</span>
+																</a>
+														    </li> 
+                                                            <li class="m-nav__item">
+																<a href="{{ URL::to('properties_settings/'.$row->id.'/property_documents')}}" class="m-nav__link">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Property Documents
+																	</span>
+																</a>
+														    </li> 
+                                                            <li class="m-nav__item">
+																<a href="{{ URL::to('properties_settings/'.$row->id.'/property_images')}}" class="m-nav__link">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Images
+																	</span>
+																</a>
+														    </li> 
+                                                            <li class="m-nav__item">
+																<a href="{{ URL::to('properties_settings/'.$row->id.'/gallery_images')}}" class="m-nav__link">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Galleries
+																	</span>
+																</a>
+														    </li> 
+                                                            <li class="m-nav__item">
+																<a href="{{URL::to('advertising')}}" class="m-nav__link">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Become Featured
+																	</span>
+																</a>
+														    </li> 
+                                                            <li class="m-nav__item">
+																<a href="https://emporium-collection.com/" class="m-nav__link" target="_blank">
+																	<i class="m-nav__link-icon"></i>
+																	<span class="m-nav__link-text">
+																		Get Help
+																	</span>
 											    </li>
 											</ul>
 										</div>
