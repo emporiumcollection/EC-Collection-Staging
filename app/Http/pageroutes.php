d84c1eb920b65a6d42df884ef7c90fc22dec5df2
riaan <riaan@Emporium-Voyage-Linode.localdomain>
2019-09-30 11:16:40 +0200

    Files Added Automatically

--- app/Http/pageroutes.php
+++ app/Http/pageroutes.php
@@ -38,4 +38,5 @@ Route::get('restaurants', 'HomeController@index');
 Route::get('membership_hotel', 'HomeController@index');
 Route::get('mapsearch', 'HomeController@index');
 Route::get('EventsMod', 'HomeController@index');
+Route::get('testRiaan', 'HomeController@index');
 ?>
